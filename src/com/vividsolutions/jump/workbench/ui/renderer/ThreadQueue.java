c3d74ef4ab4a30db551bca0db96d07938ff36c78
S L Teichmann <s-l-teichmann@users.sourceforge.net>
2007-06-22 20:53:56 +0000

    Replacement for old ThreadQueue

--- src/com/vividsolutions/jump/workbench/ui/renderer/ThreadQueue.java
+++ src/com/vividsolutions/jump/workbench/ui/renderer/ThreadQueue.java
@@ -2,7 +2,8 @@
  * The Unified Mapping Platform (JUMP) is an extensible, interactive GUI 
  * for visualizing and manipulating spatial features with geometry and attributes.
  *
- * Copyright (C) 2003 Vivid Solutions
+ * Copyright (C) 2003 Vivid Solutions 
+ * Copyright (C) 2007 Intevation GmbH
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,9 +19,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  * 
- * For more information, contact:
- *
- * Vivid Solutions
  * Suite #1A
  * 2328 Government Street
  * Victoria BC  V8T 5G5
@@ -29,84 +27,247 @@
  * (250)385-6040
  * www.vividsolutions.com
  */
-
 package com.vividsolutions.jump.workbench.ui.renderer;
 
+import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Vector;
-
 
 /**
- * Limits the number of executing threads to a specified maximum.
+ * This thread queue executes at maximum N Runnables in parallel
+ * were N is a given number of worker threads that should be used.
+ * If N threads are running and busy each further incoming 
+ * Runnable is queued until one of the threads has finished its current job.
+ * If a worker thread becomes idle (no more job in the queue)
+ * it is hold alive for 5 seconds. If during this period of time 
+ * no new Runnable is enqueued the worker thread dies.
+ *
+ * @author Sascha L. Teichmann (sascha.teichmann@intevation.de)
  */
-public class ThreadQueue {
-    private volatile int runningThreads = 0;
+public class ThreadQueue
+{
+	/** The time a worker thread stays alive if idle */
+	public static final long WORKER_STAY_ALIVE_TIME = 5000L;
 
-    //Vector is thread-safe. [Jon Aquino]
-    private Vector queuedRunnables = new Vector();
-    private int maxRunningThreads;
+	/**
+	 * Worker thread. Fetches Runnable from the surrounding 
+	 * ThreadQueue instance.
+	 */
+	protected class Worker 
+	extends         Thread
+	{
+		public void run() {
+			try {
+				for (;;) {
+					Runnable runnable;
 
-    public ThreadQueue(final int maxRunningThreads) {
-        this.maxRunningThreads = maxRunningThreads;
-    }
+					synchronized (queuedRunnables) {
+						if (queuedRunnables.isEmpty()) {
 
-    public void clear() {
-        queuedRunnables.clear();
-    }
+							++waitingThreads;
+							try {
+								queuedRunnables.wait(WORKER_STAY_ALIVE_TIME);
+							}
+							catch (InterruptedException ie) {
+							}
+							finally {
+								--waitingThreads;
+							}
 
-    private void processQueue() {
-        while (!queuedRunnables.isEmpty() &&
-                (runningThreads < maxRunningThreads) && enabled) {
-            setRunningThreads(getRunningThreads()+1);
-            new Thread((Runnable) queuedRunnables.remove(0)).start();
-            // I wonder if it would improve performance to put a little
-            // delay here. This loop seems pretty tight. Then again, I haven't
-            // worked with this code in several months, so I might be mistaken.
-            // [Jon Aquino 2005-03-2]
-        }
-    }
-    
-    private void setRunningThreads(int runningThreads) {
-        this.runningThreads = runningThreads;
-        if (runningThreads == 0) { fireAllRunningThreadsFinished(); }
-    }
-    
-    public void add(final Runnable runnable) {
-        queuedRunnables.add(new Runnable() {
-            public void run() {
-                try {
-                    runnable.run();
-                } finally {
-                    setRunningThreads(getRunningThreads() - 1);
-                    processQueue();
-                }
-            }
-        });
-        processQueue();
-    }
-    public int getRunningThreads() {
-        return runningThreads;
-    }
-    
-    private ArrayList listeners = new ArrayList();
-    public void add(Listener listener) { listeners.add(listener); }
-    public void remove(Listener listener) { listeners.remove(listener); }    
-    public static interface Listener {
-        public void allRunningThreadsFinished();
-    }
-    private void fireAllRunningThreadsFinished() {
-    	//new ArrayList to avoid ConcurrentModificationException [Jon Aquino]
-        for (Iterator i = new ArrayList(listeners).iterator(); i.hasNext(); ) {
-            Listener listener = (Listener) i.next();
-            listener.allRunningThreadsFinished();
-        }
-    }
+							// if still empty -> die!
+							if (queuedRunnables.isEmpty())
+								break;
+						}
+						if (disposed)
+							break;
+						runnable = (Runnable)queuedRunnables.remove();
+					} // synchronized queuedRunnables
 
-    public void dispose() {
-        enabled = false;
-    }
-    
-    private volatile boolean enabled = true;
+					try {
+						runnable.run();
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
 
+					// check if we are the last of the mohicans ...
+					boolean lastRunningThread;
+					synchronized (runningThreads) {
+						lastRunningThread = runningThreads[0] == 1;
+					}
+					if (lastRunningThread) {
+						fireAllRunningThreadsFinished();
+					}
+				} // for (;;)
+			}
+			finally { // guarantee that counter goes down
+				synchronized (runningThreads) {
+					--runningThreads[0];
+				}
+			}
+		}
+	} // class Worker
+
+	/**
+	 * If the number of running threads goes down to zero
+	 * implementations of this interface are able to be informed.
+	 */
+	public interface Listener {
+		void allRunningThreadsFinished();
+	} // interface Listener
+
+	/** Number of running threads */
+	protected int [] runningThreads = new int[1];
+
+	/** max. Number of threads running parallel */
+	protected int maxRunningThreads;
+
+	/** Number of threads that are currently idle */
+	protected int waitingThreads;
+
+	/** The queue of Runnables jobs waiting to be run */
+	protected LinkedList queuedRunnables;
+
+	/** Singals that the ThreadQueue is going to quit */
+	protected boolean disposed;
+
+	/** List of Listeners */
+	protected ArrayList listeners = new ArrayList();
+
+	/**
+	 * Creates a ThreadQueue with one worker thread.
+	 */
+	public ThreadQueue() {
+		this(1);
+	}
+
+	/** Creates a ThreadQueue with a given number of worker threads.
+	 * @param maxRunningThreads the max. number of threads to be run parallel.
+	 */
+	public ThreadQueue(int maxRunningThreads) {
+		this.maxRunningThreads = Math.max(1, maxRunningThreads);
+		queuedRunnables = new LinkedList();
+	}
+
+	/**
+	 * Adds a Listener to this ThreadQueue.
+	 * @param listener the listener to add.
+	 */
+	public synchronized void add(Listener listener) {
+		if (listener != null)
+			listeners.add(listener);
+	}
+
+	/**
+	 * Removes a Listener from this ThreadQueue.
+	 * @param listener the listener to be removed.
+	 */
+	public synchronized void remove(Listener listener) {
+		if (listener != null)
+			listeners.remove(listener);
+	}
+
+	/**
+	 * Informs Listeners of the fact that the number of running threads
+	 * went to zero.
+	 */
+	protected void fireAllRunningThreadsFinished() {
+		ArrayList copy;
+		synchronized (this) { copy = new ArrayList(listeners); }
+		for (int i = copy.size()-1; i >= 0; --i)
+			((Listener)copy.get(i)).allRunningThreadsFinished();
+	}
+
+
+	/**
+	 * The number of currently running worker threads.
+	 * @return number of currently running worker threads.
+	 */
+	public int runningThreads() {
+		synchronized (runningThreads) {
+			return runningThreads[0];
+		}
+	}
+
+	/**
+	 * The number of currently running worker threads.
+	 * Alias for runningThreads()
+	 * @return number of currently running worker threads.
+	 */
+	public int getRunningThreads() {
+		return runningThreads();
+	}
+
+	/**
+	 * The number of currently waiting Runnables.
+	 * @return number of currently waiting Runnables.
+	 */
+	public int waitingRunnables() {
+		synchronized (runningThreads) {
+			return queuedRunnables.size();
+		}
+	}
+
+	/**
+	 * The number of currently idle worker threads.
+	 * @return number of currently idle worker threads.
+	 */
+	public int waitingThreads() {
+		synchronized (queuedRunnables) {
+			return waitingThreads;
+		}
+	}
+
+	/**
+	 * Adds a Runnables to the queue. It will be run in one
+	 * of the worker threads.
+	 * @param runnable The Runnables to add
+	 */
+	public void add(Runnable runnable) {
+		int waiting;
+		synchronized (queuedRunnables) {
+			if (disposed)
+				return;
+			waiting = waitingThreads;
+			queuedRunnables.add(runnable);
+			queuedRunnables.notify();
+		}  // synchronized (queuedRunnables)
+
+		synchronized (runningThreads) {
+
+			// if waitingThreads == 1 then
+			// the queuedRunnables.notify() should have waked it up.
+
+			if (waitingThreads < 2 && runningThreads[0] < maxRunningThreads) {
+				++runningThreads[0];
+				Worker w = new Worker();
+				w.setDaemon(true);
+				w.start();
+			}
+		} // synchronized (runningThreads)
+	}
+
+	/**
+	 * Empties the queue of waiting Runnables.
+	 */
+	public void clear() {
+		synchronized (queuedRunnables) {
+			queuedRunnables.clear();
+		}
+	}
+
+	/**
+	 * Shuts down the ThreadQueue.
+	 */
+	public void dispose() {
+		synchronized (queuedRunnables) {
+			disposed = true;
+			queuedRunnables.clear();
+			// wakeup idle threads
+			queuedRunnables.notifyAll();
+		}
+		synchronized (this) {
+			listeners.clear();
+		}
+	}
 }
+// end of file
