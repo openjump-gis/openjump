cd17be5c23f6a48d9082c87fd29cea46dbeb8b97
Stefan Steiniger <mentaer@users.sourceforge.net>
2007-08-09 21:14:12 +0000

    moved batik-all.jar to lib folder (to later delete the batik folder)

--- scripts/openjump-unix.sh
+++ scripts/openjump-unix.sh
@@ -9,6 +9,6 @@
 
 # The next lines are required in any case
 LIB=../lib
-CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Buoy.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.7.2.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/xml-apis-ext.jar:$LIB/log4j-1.2.8.jar:$LIB/batik/batik-all.jar:$LIB/jmat_5.0m.jar:$LIB/ermapper.jar:$LIB/jai_core.jar:$LIB/jai_codec.jar:$LIB/ext
+CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Buoy.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.7.2.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/xml-apis-ext.jar:$LIB/log4j-1.2.8.jar:$LIB/batik-all.jar:$LIB/jmat_5.0m.jar:$LIB/ermapper.jar:$LIB/jai_core.jar:$LIB/jai_codec.jar:$LIB/ext
 java -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dlog4j.configuration=file:./log4j.xml -Xms256M -Xmx256M -cp $CLASSPATH com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory $LIB/ext
 
