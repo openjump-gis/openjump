84f09bad11f2bb831acf5048b21b129bed7e6b3b
Michael Michaud <michaudm@users.sourceforge.net>
2008-07-26 09:40:45 +0000

    Replace old progressiveUnion optimization by the new UnaryUnionOp of JTS 1.9 which is much faster

--- src/com/vividsolutions/jump/workbench/ui/plugin/analysis/UnionPlugIn.java
+++ src/com/vividsolutions/jump/workbench/ui/plugin/analysis/UnionPlugIn.java
@@ -44,6 +44,7 @@ import javax.swing.JComboBox;
 
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.operation.union.UnaryUnionOp;
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.feature.AttributeType;
 import com.vividsolutions.jump.feature.Feature;
@@ -51,6 +52,7 @@ import com.vividsolutions.jump.feature.FeatureCollection;
 import com.vividsolutions.jump.feature.FeatureDataset;
 import com.vividsolutions.jump.feature.FeatureDatasetFactory;
 import com.vividsolutions.jump.feature.FeatureSchema;
+import com.vividsolutions.jump.feature.FeatureUtil;
 import com.vividsolutions.jump.task.TaskMonitor;
 import com.vividsolutions.jump.workbench.model.StandardCategoryNames;
 import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
@@ -129,112 +131,20 @@ public class UnionPlugIn extends AbstractPlugIn implements ThreadedPlugIn {
                             .getFeaturesWithSelectedItems();
             FeatureSchema featureSchema = new FeatureSchema();
             featureSchema.addAttribute("GEOMETRY", AttributeType.GEOMETRY);
-		 	a = new FeatureDataset(inputC, featureSchema);
+            a = new FeatureDataset(inputC, featureSchema);
         }
         else {
             a = dialog.getLayer(LAYER).getFeatureCollectionWrapper();
         }
         
-        FeatureCollection union = progressiveUnion(monitor, a);
+        Collection geoms = FeatureUtil.toGeometries(a.getFeatures());
+        Geometry g = UnaryUnionOp.union(geoms);
+        geoms.clear();
+        geoms.add(g);
+        FeatureCollection fc = FeatureDatasetFactory.createFromGeometry(geoms);
         
         context.getLayerManager().addCategory(StandardCategoryNames.RESULT);
-        context.addLayer(StandardCategoryNames.RESULT, I18N.get("ui.plugin.analysis.UnionPlugIn.union"), union);
-        
-    }
-
-    // The naive algorithm is not efficient for dataset containing more than one thousand
-    // features. See a replacement in progressiveUnion [mmichaud 2007-06-10]
-    private FeatureCollection union(TaskMonitor monitor, FeatureCollection fc) {
-        monitor.allowCancellationRequests();
-        monitor.report(I18N.get("ui.plugin.analysis.UnionPlugIn.computing-union"));
-
-        List unionGeometryList = new ArrayList();
-
-        Geometry currUnion = null;
-        int size = fc.size();
-        int count = 1;
-
-        for (Iterator i = fc.iterator(); i.hasNext();) {
-            Feature f = (Feature) i.next();
-            Geometry geom = f.getGeometry();
-
-            if (currUnion == null) {
-                currUnion = geom;
-            } else {
-                currUnion = currUnion.union(geom);
-            }
-
-            monitor.report(count++, size, "features");
-        }
-
-        unionGeometryList.add(currUnion);
-
-        return FeatureDatasetFactory.createFromGeometry(unionGeometryList);
-    }
-    
-   /**
-    * New method for union. Instead of the naive algorithm looping over the features and
-    * unioning each time, this one union small groups of features which are closed to each
-    * other, then iterates over the result.
-    * The difference is not so important for small datasets, but for large datasets, the
-    * difference may of 5 minutes versus 5 hours.
-    */
-    private FeatureCollection progressiveUnion(TaskMonitor monitor, FeatureCollection fc) {
-        monitor.allowCancellationRequests();
-        //monitor.report(I18N.get("ui.plugin.analysis.UnionPlugIn.computing-union"));
-        
-        List unionGeometryList = new ArrayList();
-        for (Iterator i = fc.iterator(); i.hasNext();) {
-            unionGeometryList.add(((Feature) i.next()).getGeometry());
-        }
-        int iteration = 1;
-        int nbIteration = 1 + (int)(Math.log(unionGeometryList.size())/Math.log(4));
-        while (unionGeometryList.size() > 1) {
-            monitor.report(I18N.get("ui.plugin.analysis.UnionPlugIn.computing-union") + " (" + iteration++ + "/" + nbIteration + ")");
-            final int cellSize = 1 + (int)Math.sqrt(unionGeometryList.size());
-            java.util.Comparator comparator =  new java.util.Comparator(){
-                public int compare(Object o1, Object o2) {
-                    if (o1==null || o2==null) return 0;
-                    Envelope env1 = ((Geometry)o1).getEnvelopeInternal();
-                    Envelope env2 = ((Geometry)o2).getEnvelopeInternal();
-                    double indice1 = env1.getMinX()/cellSize + cellSize*((int)env1.getMinY()/cellSize);
-                    double indice2 = env2.getMinX()/cellSize + cellSize*((int)env2.getMinY()/cellSize);
-                    return indice1>=indice2?1:indice1<indice2?-1:0;
-                }
-                public boolean equals(Object obj) {return this.equals(obj);}
-            };
-            java.util.TreeSet treeSet = new java.util.TreeSet(comparator);
-            treeSet.addAll(unionGeometryList);
-            // Testes with groups of 4, 8 and 16 (4 is better than 8 which is better than 16
-            // for large datasets).
-            unionGeometryList = union(monitor, treeSet, 4);
-        }
-        
-        return FeatureDatasetFactory.createFromGeometry(unionGeometryList);
-    }
-    
-   /**
-    * Method unioning an ordered set of geometries by small groups.
-    * 
-    */
-    private List union(TaskMonitor monitor, Set set, int groupSize) {
-        List unionGeometryList = new ArrayList();
-        Geometry currUnion = null;
-        int size = set.size();
-        int count = 0;
-        for (Iterator i = set.iterator(); i.hasNext();) {
-            Geometry geom = (Geometry)i.next();
-            if (count%groupSize==0) currUnion = geom;
-            else {
-                currUnion = currUnion.union(geom);
-                if (groupSize-count%groupSize==1) unionGeometryList.add(currUnion);
-            }
-            monitor.report(++count, size, "features");
-        }
-        if (groupSize-count%groupSize!=0) {
-            unionGeometryList.add(currUnion);
-        }
-        return unionGeometryList;
+        context.addLayer(StandardCategoryNames.RESULT, I18N.get("ui.plugin.analysis.UnionPlugIn.union"), fc);
     }
     
 }
