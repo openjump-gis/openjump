5ecd85e38f016615012a977e7ff635383445e32d
Stefan Steiniger <mentaer@users.sourceforge.net>
2009-03-09 02:20:41 +0000

    Fixed bugs 2673544 - made Cut Polygon Tool undo-able

--- src/org/openjump/core/ui/plugin/edittoolbox/cursortools/CutPolygonTool.java
+++ src/org/openjump/core/ui/plugin/edittoolbox/cursortools/CutPolygonTool.java
@@ -3,6 +3,7 @@
 package org.openjump.core.ui.plugin.edittoolbox.cursortools;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -10,16 +11,21 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import org.openjump.core.apitools.FeatureCollectionTools;
+
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.feature.BasicFeature;
 import com.vividsolutions.jump.feature.Feature;
+import com.vividsolutions.jump.feature.FeatureCollection;
 import com.vividsolutions.jump.feature.FeatureUtil;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
 import com.vividsolutions.jump.workbench.model.Layer;
 import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
+import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.ui.EditTransaction;
 import com.vividsolutions.jump.workbench.ui.LayerNamePanel;
 import com.vividsolutions.jump.workbench.ui.LayerNamePanelProxy;
 import com.vividsolutions.jump.workbench.ui.LayerViewPanel;
@@ -74,10 +80,9 @@ public class CutPolygonTool extends PolygonTool {
 
 	// The user finishes to draw the polygon with the mouse
 	protected void gestureFinished() throws Exception {
-	    
 	    //Ici on va chercher la couche
 	    WorkbenchContext context = getWorkbench().getContext();
-	    
+	    this.reportNothingToUndoYet();
 	    LayerNamePanel layernamepanel = context.getLayerNamePanel();
 		
 	    Layer[] selectedLayers = layernamepanel.getSelectedLayers();
@@ -94,6 +99,7 @@ public class CutPolygonTool extends PolygonTool {
 	    
 	    else {
 	        Layer activeLayer = (Layer) selectedLayers[0];
+	    	
 	        Collection selectedFeatures = context.getLayerViewPanel().getSelectionManager() .getFeaturesWithSelectedItems(activeLayer);
 	        if (activeLayer.isEditable()) {
 	            if (!checkPolygon()) {
@@ -118,13 +124,13 @@ public class CutPolygonTool extends PolygonTool {
 								FeatureUtil.copyAttributes(featureSelected, featureDiff);	    		        	
 								featureDiff.setGeometry(newGeomDiff);
 								
-								// replace orignal polygons by cut polygons
-								activeLayer.getFeatureCollectionWrapper().remove(featureSelected);
-								activeLayer.getFeatureCollectionWrapper().add(featureIntersect);
-								activeLayer.getFeatureCollectionWrapper().add(featureDiff);
-								
-								// refresh screen
-								context.getLayerViewPanel().repaint();
+						        FeatureCollection features = activeLayer.getFeatureCollectionWrapper().getUltimateWrappee();
+						        EditTransaction edtr = new EditTransaction(new ArrayList(), "cut polygon", activeLayer, true, true, context.getLayerViewPanel());
+						        edtr.deleteFeature(featureSelected);
+						        edtr.createFeature(featureIntersect);
+						        edtr.createFeature(featureDiff);
+						        edtr.commit();
+					            edtr.clearEnvelopeCaches();
 							}
 	    			    }
 					}
@@ -134,5 +140,10 @@ public class CutPolygonTool extends PolygonTool {
 	           	JOptionPane.showMessageDialog(null,  I18N.get("ui.SchemaPanel.layer-must-be-editable"),  I18N.get("org.openjump.core.ui.plugin.edittoolbox.Information"), JOptionPane.INFORMATION_MESSAGE);
 	        }
 	    }
+	}
+	
+	protected boolean isRollingBackInvalidEdits(WorkbenchContext context) {
+		return context.getWorkbench().getBlackboard()
+				.get(EditTransaction.ROLLING_BACK_INVALID_EDITS_KEY, false);
 	}
 }
