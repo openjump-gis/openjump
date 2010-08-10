8924128cde97d0cb197296879a5a95da1cdac7b9
Stefan Steiniger <mentaer@users.sourceforge.net>
2010-08-10 19:29:00 +0000

    added code back that decides on loading into memory or still reading from file

--- src/org/openjump/core/rasterimage/RasterImageLayer.java
+++ src/org/openjump/core/rasterimage/RasterImageLayer.java
@@ -89,6 +89,10 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
     protected static long availRAM = Runtime.getRuntime().maxMemory();
     protected static double freeRamFactor = 0.5;
     protected static double minRamToKeepFree = availRAM * freeRamFactor;
+    //[sstein 9.Aug.2010]
+    // The value below is set dynamically based on available memory
+    //       now its 200x200px as min (originally it was 500x500)
+    //protected static int maxPixelsForFastDisplayMode = 40000;
     protected static int maxPixelsForFastDisplayMode = 250000;
 
     protected String imageFileName = null;
@@ -99,6 +103,7 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
     //-- [sstein 2nd Aug 2010] new, since we scale the image now for display
     protected static Raster rasterData = null;
     protected boolean rasterDataChanged = false; //may be needed for rescaling the image values
+    protected boolean wasScaledForDisplay = false;
     //-- end
     
     protected PlanarImage imageProcessingStep1 = null, imageProcessingStep2 = null;
@@ -152,6 +157,15 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
             this.setImage(javax.media.jai.PlanarImage.wrapRenderedImage(imageToDisplay));
         if (newRaster != null)
         	this.setRasterData(newRaster);
+        //[sstein 9.Aug.2010]       
+        long avram = getAvailRAM();
+        if(avram > 256000000){
+        	maxPixelsForFastDisplayMode = 250000; //500x500 px
+        }
+        if(avram > 750000000){
+        	maxPixelsForFastDisplayMode = 4000000; //2000x2000 px
+        }
+        //[sstein end]
     }
     
     
@@ -183,6 +197,15 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
         else{
             //logger.printError("given raster is NULL");
         }
+        //[sstein 9.Aug.2010]
+        long avram = getAvailRAM();
+        if(avram > 256000000){
+        	maxPixelsForFastDisplayMode = 250000; //500x500 px
+        }
+        if(avram > 750000000){
+        	maxPixelsForFastDisplayMode = 563500; //750x750 px
+        }
+        //[sstein end]
     }
 
     /**
@@ -298,13 +321,32 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
                 this.visibleEnv = newVisibleEnv;
                 
                 if ( (this.origImageWidth * this.origImageHeight) < RasterImageLayer.getMaxPixelsForFastDisplayMode() ){
-                    
-                    // faster display (uses more RAM) for small images
+                    //int pixels = this.origImageWidth * this.origImageHeight;
+                	//int maxpixels = RasterImageLayer.getMaxPixelsForFastDisplayMode();
+                    //-- faster display (uses more RAM) for small images
                     this.setImageProcessingMode(RasterImageLayer.MODE_FASTDISPLAY);
                     
                     if (this.isImageNull()){
                         this.reLoadImage();
                     }
+                    //[sstein 9.Aug.2010]
+                    //problem: putting the stuff below here will also avoid the first re-scaling of the image
+                    //         for better display, so I added the additional scaling stuff
+                    
+                    long totalMem = Runtime.getRuntime().totalMemory();
+                    long freeMem = Runtime.getRuntime().freeMemory();
+                    long committedMemory = totalMem - freeMem;
+                    double maxMemoryToCommit = availRAM - minRamToKeepFree;
+                    boolean needFreeRAM = (committedMemory > maxMemoryToCommit);
+                    if(needFreeRAM == false){
+                    	if (wasScaledForDisplay == false){
+                    		setImage(stretchImageValuesForDisplay(getImageForDisplay()));
+                    		wasScaledForDisplay = true;
+                    	}
+                    	this.setNeedToKeepImage(true); //so small images are not reloaded every time
+                    }
+                    
+                    //[sstein end]
                     
                     this.imagePart = this.getVisibleImageCoordinatesOfImage( this.origImageWidth, this.origImageHeight, this.visibleEnv, this.getEnvelope() );
                     
@@ -441,15 +483,19 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
     }
     
     /**
-     * deletes image from RAM (if it is not to be keeped and if the RAM consumption is high)
+     * deletes image from RAM (if it is not to be kept and if the RAM consumption is high)
      * and calls the garbage collector, if the <code>garbageCollect</code> is true.
      *@param garbageCollect if true the garbage collector will be called (this parameter may be overridden, if there is not enough RAM available...)
      */
     public boolean clearImageAndRaster(boolean garbageCollect){
+    	//TODO: [sstein 9.Aug.2010] not sure if below condition is correct, since it 
+    	//       does not account for Xmx (max memory), only for the actual memory
+    	//       Hence we should work with committed memory as I did above??? 
         boolean reallyNeedToFreeRAM = (Runtime.getRuntime().freeMemory() < minRamToKeepFree);
         if (!this.needToKeepImage && reallyNeedToFreeRAM ){
             this.image = null;
             rasterData = null; //[sstein 2Aug2010] line added
+            this.wasScaledForDisplay = false; //[sstein 20Aug2010] line added
         }
         if (garbageCollect){
             Runtime.getRuntime().gc();
@@ -638,11 +684,6 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
                 rasterData = rData;
                 //-- sstein end
 
-                // This returns a planaimage with actual pixel values
-//                if (pImage != null) {
-//                    return pImage;
-//                }
-
                 // This rescales values
                 // See http://www.lac.inpe.br/JIPCookbook/2200-display-surrogate.jsp
 
@@ -694,11 +735,6 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
        		 	Raster rData = pImage.copyData();  //copy data so we do not get a ref
                 rasterData = rData;
                 //-- sstein end
-
-                // This returns a planaimage with actual pixel values
-//                if (pImage != null) {
-//                    return pImage;
-//                }
 
                 // This rescales values
                 // See http://www.lac.inpe.br/JIPCookbook/2200-display-surrogate.jsp
@@ -1550,7 +1586,7 @@ public class RasterImageLayer extends AbstractLayerable implements ObjectContain
 	 */
 	public Raster getRasterData() {
         //if (rasterData == null)
-		//-- [sstein Aug9th 2010] now reload every time but keep Style
+		//-- [sstein Aug10th 2010] now reload every time but keep Style
         this.reLoadImageButKeepImageForDisplay();
 		return rasterData;
 	}
