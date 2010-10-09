10a637707995fcb1fbc6d19a2d56e63740e2ac78
Michael Michaud <michaudm@users.sourceforge.net>
2010-10-09 14:48:17 +0000

    ExtractLayerByGeometry : document the semantic for empty geometries and creates one more layer for empty geometry collections

--- src/org/openjump/core/ui/plugin/layer/ExtractLayersByGeometry.java
+++ src/org/openjump/core/ui/plugin/layer/ExtractLayersByGeometry.java
@@ -51,7 +51,7 @@ import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.feature.Feature;
-import com.vividsolutions.jump.feature.FeatureCollectionWrapper;
+import com.vividsolutions.jump.feature.FeatureCollection;
 import com.vividsolutions.jump.feature.FeatureDataset;
 import com.vividsolutions.jump.feature.FeatureSchema;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
@@ -66,12 +66,32 @@ import com.vividsolutions.jump.workbench.ui.MenuNames;
 import com.vividsolutions.jump.workbench.ui.images.IconLoader;
 import com.vividsolutions.jump.workbench.ui.plugin.FeatureInstaller;
 
+/**
+ * This plugin split a layer into several layers when its features have
+ * different geometry dimensions.
+ * It can produce a maximum of 4 layers
+ * <ul>
+ * <li>Empty geometries</li>
+ * <li>Punctual geometries</li>
+ * <li>Linear geometries</li>
+ * <li>Polygonal geometries</li>
+ * </ul>
+ * Rules followed to spread features into the 4 layers :
+ * <ul>
+ * <li>Empty GeometryCollection's are copied into empty-geometries layer</li>
+ * <li>Non-empty GeometryCollections are exploded into simple geometry features</li>
+ * <li>Empty simple features are copied into the layer corresponding to their dimension</li>
+ * <li>Non-empty simple features are copied into the layer corresponding to their dimension</li>
+ * </ul>
+ */ 
 public class ExtractLayersByGeometry extends AbstractPlugIn {
 
 	   private final static String EXTRACT_LAYERS_BY_GEOMETRY_TYPE = 
 	    	I18N.get("org.openjump.core.ui.plugin.layer.ExtractLayersByGeometry.Extract-Layers-by-Geometry-Type");
 	   private final static String ONLY_ONE_GEOMETRY_TYPE_FOUND = 
 	    	I18N.get("org.openjump.core.ui.plugin.layer.ExtractLayersByGeometry.Only-one-geometry-type-found");
+	   private final static String EMPTY = 
+	    	I18N.get("org.openjump.core.ui.plugin.layer.ExtractLayersByGeometry.empty");
 	   private final static String POINT = 
 	    	I18N.get("org.openjump.core.ui.plugin.layer.ExtractLayersByGeometry.point");
 	   private final static String LINE = 
@@ -110,8 +130,7 @@ public class ExtractLayersByGeometry extends AbstractPlugIn {
 				createEnableCheck(context.getWorkbenchContext()));
 	}
 
-    public static MultiEnableCheck createEnableCheck(WorkbenchContext workbenchContext)
-    {
+    public static MultiEnableCheck createEnableCheck(WorkbenchContext workbenchContext) {
         EnableCheckFactory checkFactory = new EnableCheckFactory(workbenchContext);      
         return new MultiEnableCheck()
         .add(checkFactory.createWindowWithSelectionManagerMustBeActiveCheck())
@@ -123,7 +142,7 @@ public class ExtractLayersByGeometry extends AbstractPlugIn {
 		Layer[] layers = context.getWorkbenchContext().getLayerNamePanel().getSelectedLayers();
 		if (layers.length > 0){
 			Layer layer = layers[0];
-			if (!CompatibleFeatures(layer)) 
+			if (!compatibleFeatures(layer)) 
 				splitLayer(context, layer);
 			else
 				context.getWorkbenchFrame().warnUser(ONLY_ONE_GEOMETRY_TYPE_FOUND);
@@ -141,75 +160,80 @@ public class ExtractLayersByGeometry extends AbstractPlugIn {
 
     private List splitLayer(PlugInContext context, Layer layer)
     {
-    	ArrayList newLayers = new ArrayList();
+        ArrayList newLayers = new ArrayList();
     	
-            ArrayList pointFeatures = new ArrayList();
-            ArrayList lineFeatures = new ArrayList();
-            ArrayList polyFeatures = new ArrayList();
-            
-    		FeatureCollectionWrapper featureCollection = layer.getFeatureCollectionWrapper();
-            List featureList = featureCollection.getFeatures();
-            FeatureSchema featureSchema = layer.getFeatureCollectionWrapper().getFeatureSchema();
-            			          
-            featureCollection = layer.getFeatureCollectionWrapper();
-            featureList = layer.getFeatureCollectionWrapper().getFeatures();
+        ArrayList emptyFeatures = new ArrayList();
+        ArrayList pointFeatures = new ArrayList();
+        ArrayList lineFeatures = new ArrayList();
+        ArrayList polyFeatures = new ArrayList();
+        
+    	FeatureCollection featureCollection = layer.getFeatureCollectionWrapper();
+        List featureList = featureCollection.getFeatures();
+        FeatureSchema featureSchema = featureCollection.getFeatureSchema();
 
-            Collection selectedCategories = context.getLayerNamePanel().getSelectedCategories();
-            
-            for (Iterator i = featureList.iterator(); i.hasNext();)
-            {
-            	Feature feature = (Feature) i.next();
-            	Geometry geo = feature.getGeometry();
-            	BitSet currFeatureBit = new BitSet();
-            	currFeatureBit = setBit(currFeatureBit, geo);
-            	if (geo instanceof GeometryCollection) {
-            		explodeGeometryCollection(featureSchema, pointFeatures, lineFeatures, 
-            				polyFeatures, (GeometryCollection) geo, feature);
-            	} else if  (currFeatureBit.get(pointBit)) {
+        Collection selectedCategories = context.getLayerNamePanel().getSelectedCategories();
+        
+        for (Iterator i = featureList.iterator(); i.hasNext();) {
+            Feature feature = (Feature) i.next();
+            Geometry geo = feature.getGeometry();
+            BitSet currFeatureBit = new BitSet();
+            currFeatureBit = setBit(currFeatureBit, geo);
+            if (geo instanceof GeometryCollection) {
+                explodeGeometryCollection(featureSchema, emptyFeatures, pointFeatures,
+                        lineFeatures, polyFeatures, (GeometryCollection) geo, feature);
+            } else if  (currFeatureBit.get(pointBit)) {
             		pointFeatures.add(feature.clone(true));
-            	} else if  (currFeatureBit.get(lineBit)) {
+            } else if  (currFeatureBit.get(lineBit)) {
             		lineFeatures.add(feature.clone(true));
-            	} else if (currFeatureBit.get(polyBit)) {
+            } else if (currFeatureBit.get(polyBit)) {
             		polyFeatures.add(feature.clone(true));
-            	}
             }
-            
-            if (pointFeatures.size() > 0)
-            {
-		        Layer pointLayer = context.addLayer(selectedCategories.isEmpty()
+        }
+        
+        if (emptyFeatures.size() > 0) {
+		    Layer emptyLayer = context.addLayer(selectedCategories.isEmpty()
+		        ? StandardCategoryNames.RESULT
+		        : selectedCategories.iterator().next().toString(), layer.getName() + "_" + EMPTY,
+		        new FeatureDataset(featureSchema));
+		    emptyLayer.setStyles(layer.cloneStyles());		        
+		    FeatureCollection emptyFeatureCollection = emptyLayer.getFeatureCollectionWrapper();
+		    newLayers.add(emptyLayer);	           
+	        emptyFeatureCollection.addAll(emptyFeatures);
+        }
+        
+        if (pointFeatures.size() > 0) {
+		    Layer pointLayer = context.addLayer(selectedCategories.isEmpty()
 		        ? StandardCategoryNames.RESULT
 		        : selectedCategories.iterator().next().toString(), layer.getName() + "_" + POINT,
 		        new FeatureDataset(featureSchema));
-		        pointLayer.setStyles(layer.cloneStyles());		        
-		        FeatureCollectionWrapper pointFeatureCollection = pointLayer.getFeatureCollectionWrapper();
-		        newLayers.add(pointLayer);	           
-	            pointFeatureCollection.addAll(pointFeatures);
-           }
-            
-            if (lineFeatures.size() > 0)
-            {
- 		        Layer lineLayer = context.addLayer(selectedCategories.isEmpty()
+		    pointLayer.setStyles(layer.cloneStyles());		        
+		    FeatureCollection pointFeatureCollection = pointLayer.getFeatureCollectionWrapper();
+		    newLayers.add(pointLayer);	           
+	        pointFeatureCollection.addAll(pointFeatures);
+        }
+        
+        if (lineFeatures.size() > 0) {
+ 		    Layer lineLayer = context.addLayer(selectedCategories.isEmpty()
 				? StandardCategoryNames.RESULT
 				: selectedCategories.iterator().next().toString(), layer.getName() + "_" + LINE,
 				new FeatureDataset(featureSchema));
-				lineLayer.setStyles(layer.cloneStyles());				
-		        FeatureCollectionWrapper lineFeatureCollection = lineLayer.getFeatureCollectionWrapper();
-		        newLayers.add(lineLayer);	           
-				lineFeatureCollection.addAll(lineFeatures);
-           }
-            
-            if (polyFeatures.size() > 0)
-            {
- 		        Layer polyLayer = context.addLayer(selectedCategories.isEmpty()
+			lineLayer.setStyles(layer.cloneStyles());				
+		    FeatureCollection lineFeatureCollection = lineLayer.getFeatureCollectionWrapper();
+		    newLayers.add(lineLayer);	           
+			lineFeatureCollection.addAll(lineFeatures);
+        }
+        
+        if (polyFeatures.size() > 0) {
+ 		    Layer polyLayer = context.addLayer(selectedCategories.isEmpty()
 				? StandardCategoryNames.RESULT
 				: selectedCategories.iterator().next().toString(), layer.getName() + "_" + AREA,
 				new FeatureDataset(featureSchema));
-		        polyLayer.setStyles(layer.cloneStyles());				        
-		        FeatureCollectionWrapper polyFeatureCollection = polyLayer.getFeatureCollectionWrapper();
-		        newLayers.add(polyLayer);	           
-		        polyFeatureCollection.addAll(polyFeatures);
-           }
-     		context.getLayerViewPanel().repaint();
+		    polyLayer.setStyles(layer.cloneStyles());				        
+		    FeatureCollection polyFeatureCollection = polyLayer.getFeatureCollectionWrapper();
+		    newLayers.add(polyLayer);	           
+		    polyFeatureCollection.addAll(polyFeatures);
+        }
+     	context.getLayerViewPanel().repaint();
     	return newLayers;
     }
 
@@ -218,64 +242,70 @@ public class ExtractLayersByGeometry extends AbstractPlugIn {
 	static final int lineBit = 2;
 	static final int polyBit = 3;
 	
-    private void explodeGeometryCollection(FeatureSchema fs, ArrayList pointFeatures, ArrayList lineFeatures, 
-    		ArrayList polyFeatures, GeometryCollection geometryCollection, Feature feature)
-    {
-    	for (int i = 0; i < geometryCollection.getNumGeometries(); i++)
-    	{
-    		Geometry geometry = geometryCollection.getGeometryN(i);
+    private void explodeGeometryCollection(FeatureSchema fs,
+                                           ArrayList emptyFeatures,
+                                           ArrayList pointFeatures,
+                                           ArrayList lineFeatures, 
+                                           ArrayList polyFeatures,
+                                           GeometryCollection geometryCollection,
+                                           Feature feature) {
+        
+        if (geometryCollection.isEmpty()) {
+            emptyFeatures.add(feature.clone(false));
+        }
+        else {
+    	    for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+    		    Geometry geometry = geometryCollection.getGeometryN(i);
     		
-    		if (geometry instanceof GeometryCollection)
-    		{
-    			explodeGeometryCollection(fs, pointFeatures, lineFeatures, polyFeatures, 
-    					(GeometryCollection) geometry, feature);
-    		}
-    		else
-    		{
-    			Feature newFeature = feature.clone(false);
-    			newFeature.setGeometry((Geometry) geometry.clone());
-    			BitSet featureBit = new BitSet();
-    			featureBit = setBit(featureBit, geometry);
-    			if (featureBit.get(pointBit)) {
-    				pointFeatures.add(newFeature);
-    			} else if (featureBit.get(lineBit)) { 
-    				lineFeatures.add(newFeature);
-    			} else if (featureBit.get(polyBit)) {
-    				polyFeatures.add(newFeature);
-    			}
+    		    if (geometry instanceof GeometryCollection) {
+    			    explodeGeometryCollection(fs, emptyFeatures, pointFeatures, lineFeatures,
+    			            polyFeatures, (GeometryCollection)geometry, feature);
+    		    }
+    		    else {
+    			    Feature newFeature = feature.clone(false);
+    			    newFeature.setGeometry((Geometry) geometry.clone());
+    			    BitSet featureBit = new BitSet();
+    			    featureBit = setBit(featureBit, geometry);
+    			    if (featureBit.get(pointBit)) {
+    			    	pointFeatures.add(newFeature);
+    			    } else if (featureBit.get(lineBit)) { 
+    			    	lineFeatures.add(newFeature);
+    			    } else if (featureBit.get(polyBit)) {
+    			    	polyFeatures.add(newFeature);
+    			    }
+    		    }
     		}
     	}
     }
 
-    private boolean CompatibleFeatures(Layer layer)
-    {
+    private boolean compatibleFeatures(Layer layer) {
         BitSet bitSet = new BitSet();        
-        FeatureCollectionWrapper featureCollection = layer.getFeatureCollectionWrapper();
+        FeatureCollection featureCollection = layer.getFeatureCollectionWrapper();
         List featureList = featureCollection.getFeatures();
-        
-        for (Iterator i = featureList.iterator(); i.hasNext();)
+        for (Iterator i = featureList.iterator(); i.hasNext();) {
             bitSet = setBit(bitSet, ((Feature) i.next()).getGeometry());
-
+        }
         return (bitSet.cardinality() < 2);
     }
     
 
-	private static BitSet setBit(BitSet bitSet, Geometry geometry)
-    {
+	private static BitSet setBit(BitSet bitSet, Geometry geometry) {
         BitSet newBitSet = (BitSet) bitSet.clone();
-        if      (geometry.isEmpty())                  newBitSet.set(emptyBit);
-        else if (geometry instanceof Point)           newBitSet.set(pointBit);
+        if (geometry instanceof Point)                newBitSet.set(pointBit);
         else if (geometry instanceof MultiPoint)      newBitSet.set(pointBit);
         else if (geometry instanceof LineString)      newBitSet.set(lineBit);
         else if (geometry instanceof LinearRing)      newBitSet.set(lineBit);
         else if (geometry instanceof MultiLineString) newBitSet.set(lineBit);
         else if (geometry instanceof Polygon)         newBitSet.set(polyBit);
         else if (geometry instanceof MultiPolygon)    newBitSet.set(polyBit);
-        else if (geometry instanceof GeometryCollection)
-        {
-            GeometryCollection geometryCollection = (GeometryCollection) geometry;
-            for (int i = 0; i < geometryCollection.getNumGeometries(); i++)
-                newBitSet = setBit(newBitSet, geometryCollection.getGeometryN(i));
+        else if (geometry instanceof GeometryCollection) {
+            if (geometry.isEmpty())                   newBitSet.set(emptyBit);
+            else {
+                GeometryCollection geometryCollection = (GeometryCollection) geometry;
+                for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+                    newBitSet = setBit(newBitSet, geometryCollection.getGeometryN(i));
+                }
+            }
         }
         return newBitSet;
     }
