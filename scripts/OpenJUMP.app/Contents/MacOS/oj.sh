988b8de8be92bf8117eee989500f3914f6b52da9
Edgar Soldin <edso@users.sourceforge.net>
2010-10-28 22:09:04 +0000

    survives spaces in the path now

--- scripts/OpenJUMP.app/Contents/MacOS/oj.sh
+++ scripts/OpenJUMP.app/Contents/MacOS/oj.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ## open a terminal and run the mac shell script in it
-#open -a Terminal `dirname $0`/../../../oj_macosx.command
+#open -a Terminal "$(dirname "$0")/../../../oj_macosx.command"
 ## run shell script in background
-sh `dirname $0`/../../../oj_macosx.command
+sh "$(dirname "$0")/../../../oj_macosx.command"
