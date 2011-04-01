790431109ba8e911859217a23d969a3d2cc8a02d
Larry Reeder <lreeder@users.sourceforge.net>
2011-04-01 04:01:28 +0000

    Updated WKBGeometryValueConverter to handle geometry types represented by WKB hex string so that ST_AsBinary is optional

--- src/com/vividsolutions/jump/datastore/postgis/PostgisValueConverterFactory.java
+++ src/com/vividsolutions/jump/datastore/postgis/PostgisValueConverterFactory.java
@@ -69,8 +69,36 @@ public class PostgisValueConverterFactory
         throws IOException, SQLException, ParseException
     {
       byte[] bytes = rs.getBytes(columnIndex);
-      if (bytes == null) return wktReader.read("GEOMETRYCOLLECTION EMPTY");
-      else return wkbReader.read(bytes);
+
+      //so rs.getBytes will be one of two things:
+      //1. The actual bytes of the WKB if someone did ST_AsBinary
+      //2. The bytes of hex representation of the WKB.
+
+      //in the case of #1, according to the WKB spec, the byte value
+      //can only be 0 or 1.
+      //in the case of #2, it's a hex string, so values range from ascii 0-F
+      //use this logic to determine how to process the bytes.
+
+      Geometry geometry = null;
+      if(bytes == null || bytes.length <= 0)
+      {
+         geometry = wktReader.read("GEOMETRYCOLLECTION EMPTY");
+      }
+      else
+      {
+          //assume it's the actual bytes (from ST_AsBinary)
+          byte[] realWkbBytes = bytes;
+          if(bytes[0] >= '0')
+          {
+              //ok, it's hex, convert hex string to actual bytes
+              String hexString = new String(bytes);
+              realWkbBytes = WKBReader.hexToBytes(hexString);
+          }
+
+          geometry = wkbReader.read(realWkbBytes);
+      }
+
+      return geometry;
     }
   }
 }
