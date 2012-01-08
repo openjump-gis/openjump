6610dd1ad029022f504243e6c88fe8b15425eab7
Stefan Steiniger <mentaer@users.sourceforge.net>
2012-01-08 12:34:31 +0000

    delayed tagging of version 1.4.2 based on commit history

---
+++ 1.4.2/src/org/openjump/core/ui/plugin/datastore/AddDataStoreLayerWizard.java
@@ -0,0 +1,139 @@
+package org.openjump.core.ui.plugin.datastore;
+
+import java.awt.Color;
+import java.util.Collection;
+
+import javax.swing.SwingUtilities;
+
+import org.openjump.core.ui.plugin.file.open.ChooseProjectPanel;
+import org.openjump.core.ui.swing.wizard.AbstractWizardGroup;
+
+import com.vividsolutions.jump.I18N;
+import com.vividsolutions.jump.coordsys.CoordinateSystemRegistry;
+import com.vividsolutions.jump.feature.FeatureCollection;
+import com.vividsolutions.jump.io.datasource.Connection;
+import com.vividsolutions.jump.io.datasource.DataSource;
+import com.vividsolutions.jump.io.datasource.DataSourceQuery;
+import com.vividsolutions.jump.task.TaskMonitor;
+import com.vividsolutions.jump.workbench.WorkbenchContext;
+import com.vividsolutions.jump.workbench.datastore.ConnectionDescriptor;
+import com.vividsolutions.jump.workbench.model.Category;
+import com.vividsolutions.jump.workbench.model.Layer;
+import com.vividsolutions.jump.workbench.model.LayerManager;
+import com.vividsolutions.jump.workbench.model.StandardCategoryNames;
+import com.vividsolutions.jump.workbench.ui.images.IconLoader;
+import com.vividsolutions.jump.workbench.ui.plugin.AddNewLayerPlugIn;
+import com.vividsolutions.jump.workbench.ui.plugin.datastore.AddDatastoreLayerPanel;
+import com.vividsolutions.jump.workbench.ui.plugin.datastore.DataStoreDataSource;
+import com.vividsolutions.jump.workbench.ui.wizard.WizardDialog;
+
+public class AddDataStoreLayerWizard extends AbstractWizardGroup {
+  private static final String KEY = AddDataStoreLayerWizard.class.getName();
+
+  private AddDataStoreLayerWizardPanel dataStoreWizardPanel;
+
+  private WorkbenchContext workbenchContext;
+
+  private ChooseProjectPanel chooseProjectPanel;
+
+  public AddDataStoreLayerWizard(WorkbenchContext workbenchContext) {
+    super(I18N.get(KEY), IconLoader.icon("database_add.png"),
+      AddDataStoreLayerWizardPanel.class.getName());
+    this.workbenchContext = workbenchContext;
+    dataStoreWizardPanel = new AddDataStoreLayerWizardPanel(workbenchContext);
+    addPanel(dataStoreWizardPanel);
+    chooseProjectPanel = new ChooseProjectPanel(workbenchContext,
+      dataStoreWizardPanel.getID());
+    addPanel(chooseProjectPanel);
+  }
+
+  public String getFirstId() {
+    String firstId = super.getFirstId();
+    if (!chooseProjectPanel.hasActiveTaskFrame()
+      && chooseProjectPanel.hasTaskFrames()) {
+      chooseProjectPanel.setNextID(firstId);
+      return chooseProjectPanel.getID();
+    } else {
+      return firstId;
+    }
+  }
+  
+  public void run(WizardDialog dialog, TaskMonitor monitor) throws Exception {
+    chooseProjectPanel.activateSelectedProject();
+    //try {
+      AddDatastoreLayerPanel dataStorePanel = dataStoreWizardPanel.getDataStorePanel();
+      final Layer layer = createLayer(dataStorePanel, monitor);
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          Collection<Category> selectedCategories = workbenchContext.getLayerNamePanel()
+            .getSelectedCategories();
+          LayerManager layerManager = workbenchContext.getLayerManager();
+          String categoryName = StandardCategoryNames.WORKING;
+          if (!selectedCategories.isEmpty()) {
+            categoryName = selectedCategories.iterator().next().getName();
+          }
+          try {workbenchContext.getLayerViewPanel().getViewport().update();
+          } catch(Exception e) {}
+          layerManager.addLayerable(categoryName, layer);
+        }
+      });
+      workbenchContext.getLayerViewPanel().getViewport().update();
+    //} catch (Exception e) {
+    //  monitor.report(e);
+    //  throw e;
+    //}
+  }
+
+  private Layer createLayer(final AddDatastoreLayerPanel panel,
+    TaskMonitor monitor) throws Exception {
+
+    String datasetName = panel.getDatasetName();
+    LayerManager layerManager = workbenchContext.getLayerManager();
+    Color fillColor = layerManager.generateLayerFillColor();
+    FeatureCollection featureCollection = AddNewLayerPlugIn.createBlankFeatureCollection();
+    Layer layer = new Layer(datasetName, fillColor, featureCollection,
+      layerManager);
+
+    String geometryAttributeName = panel.getGeometryAttributeName();
+    String whereClause = panel.getWhereClause();
+    ConnectionDescriptor connectionDescriptor = panel.getConnectionDescriptor();
+    boolean caching = panel.isCaching();
+    DataStoreDataSource ds = new DataStoreDataSource(datasetName,
+      geometryAttributeName, whereClause, connectionDescriptor, caching,
+      workbenchContext);
+
+    DataSourceQuery dsq = new DataSourceQuery(ds, null, datasetName);
+
+    layer.setDataSourceQuery(dsq);
+
+    CoordinateSystemRegistry crsRegistry = CoordinateSystemRegistry.instance(workbenchContext.getBlackboard());
+    try {
+        layerManager.setFiringEvents(false); // added by michaudm on 2009-04-05
+        load(layer, crsRegistry, monitor);
+        layerManager.setFiringEvents(true); // added by michaudm on 2009-04-05
+    }
+    finally {layerManager.setFiringEvents(true);}
+    return layer;
+  }
+
+  public static void load(Layer layer, CoordinateSystemRegistry registry,
+    TaskMonitor monitor) throws Exception {
+    layer.setFeatureCollection(executeQuery(layer.getDataSourceQuery()
+      .getQuery(), layer.getDataSourceQuery().getDataSource(), registry,
+      monitor));
+    layer.setFeatureCollectionModified(false);
+  }
+
+  private static FeatureCollection executeQuery(String query,
+    DataSource dataSource, CoordinateSystemRegistry registry,
+    TaskMonitor monitor) throws Exception {
+    Connection connection = dataSource.getConnection();
+    try {
+      return dataSource.installCoordinateSystem(connection.executeQuery(query,
+        monitor), registry);
+    } finally {
+      connection.close();
+    }
+  }
+
+}
---
+++ 1.4.2/src/org/openjump/core/ui/plugin/edittoolbox/cursortools/DrawConstrainedLineStringTool.java
@@ -0,0 +1,113 @@
+
+/*
+ * The Unified Mapping Platform (JUMP) is an extensible, interactive GUI 
+ * for visualizing and manipulating spatial features with geometry and attributes.
+ *
+ * JUMP is Copyright (C) 2003 Vivid Solutions
+ *
+ * This program implements extensions to JUMP and is
+ * Copyright (C) 2004 Integrated Systems Analysts, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * For more information, contact:
+ *
+ * Integrated Systems Analysts, Inc.
+ * 630C Anchors St., Suite 101
+ * Fort Walton Beach, Florida
+ * USA
+ *
+ * (850)862-7321
+ */
+
+package org.openjump.core.ui.plugin.edittoolbox.cursortools;
+
+import java.awt.geom.NoninvertibleTransformException;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.operation.valid.IsValidOp;
+import com.vividsolutions.jump.I18N;
+import com.vividsolutions.jump.workbench.ui.EditTransaction;
+import com.vividsolutions.jump.workbench.ui.LayerNamePanelProxy;
+import com.vividsolutions.jump.workbench.ui.cursortool.CursorTool;
+import com.vividsolutions.jump.workbench.ui.cursortool.editing.FeatureDrawingUtil;
+
+public class DrawConstrainedLineStringTool extends ConstrainedMultiClickTool {
+    private FeatureDrawingUtil featureDrawingUtil;
+    final static String drawConstrainedLineString =I18N.get("org.openjump.core.ui.plugin.edittoolbox.cursortools.DrawConstrainedLineStringTool.Draw-Constrained-LineString");
+    final static String TheLinestringMustHaveAtLeast2Points =I18N.get("org.openjump.core.ui.plugin.edittoolbox.cursortools.DrawConstrainedLineStringTool.The-linestring-must-have-at-least-2-points");
+    
+    protected DrawConstrainedLineStringTool(FeatureDrawingUtil featureDrawingUtil) {
+        drawClosed = false;
+        this.featureDrawingUtil = featureDrawingUtil;
+    }
+
+    public static CursorTool create(LayerNamePanelProxy layerNamePanelProxy) {
+        FeatureDrawingUtil featureDrawingUtil = new FeatureDrawingUtil(layerNamePanelProxy);
+
+        return featureDrawingUtil.prepare(new DrawConstrainedLineStringTool(
+                featureDrawingUtil), true);
+    }
+
+    public String getName() {
+    	//Specify name explicitly, otherwise it will be "Draw Line String" [Jon Aquino]
+        return drawConstrainedLineString;
+    }
+
+    public Icon getIcon() {
+        return new ImageIcon(getClass().getResource("DrawLinestringConstrained.gif"));
+    }
+
+    protected void gestureFinished() throws Exception {
+        reportNothingToUndoYet();
+
+        if (!checkLineString()) {
+            return;
+        }
+
+        execute(featureDrawingUtil.createAddCommand(getLineString(),
+                isRollingBackInvalidEdits(), getPanel(), this));
+    }
+
+    protected LineString getLineString() throws NoninvertibleTransformException {
+        return new GeometryFactory().createLineString(toArray(
+                getCoordinates()));
+    }
+
+    protected boolean checkLineString() throws NoninvertibleTransformException {
+        if (getCoordinates().size() < 2) {
+            getPanel().getContext().warnUser(TheLinestringMustHaveAtLeast2Points);
+
+            return false;
+        }
+
+        IsValidOp isValidOp = new IsValidOp(getLineString());
+
+        if (!isValidOp.isValid()) {
+            getPanel().getContext().warnUser(isValidOp.getValidationError()
+                                                      .getMessage());
+
+            if (getWorkbench().getBlackboard().get(EditTransaction.ROLLING_BACK_INVALID_EDITS_KEY, false)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
