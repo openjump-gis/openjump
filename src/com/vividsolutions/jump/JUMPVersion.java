5f5b5a3fcc970141ca31f1138d65583d1219186d
Edgar Soldin <edso@users.sourceforge.net>
2012-01-17 19:56:38 +0000

    streamline versioning and display language/jump.properties is now the sole version authority display is now like in readme version release rev.revision  e.g.  '1.5.1 stable frev.1234'

--- src/com/vividsolutions/jump/JUMPVersion.java
+++ src/com/vividsolutions/jump/JUMPVersion.java
@@ -35,82 +35,30 @@ package com.vividsolutions.jump;
  * JUMP version information.
  * <p>
  * Versions consist of a 3-part version number: <code>major.minor.patch</code>
- * An optional release status string may be present in the string version of
- * the version.
- *
+ * An optional release status string may be present in the string version of the
+ * version.
+ * 
  * @version 1.5.0
  */
 public class JUMPVersion {
 
   /**
-   * The current version number of the JTS API.
+   * The current version number of the OJ.
    */
-  public static final JUMPVersion CURRENT_VERSION = new JUMPVersion();
-
-  /**
-   * The major version number.
-   */
-  //public static final int MAJOR = 1;
-
-  /**
-   * The minor version number.
-   */
-  //public static final int MINOR = 5;
-
-  /**
-   * The patch version number.
-   */
-  //public static final int PATCH = 0;
-
-  /**
-   * An optional string providing further release info (such as "alpha 1" or
-   * svn version);
-   */
-  //private static final String releaseInfo = "alpha";
-
-  /**
-   * Prints the current JTS version to stdout.
-   *
-   * @param args the command-line arguments (none are required).
-   */
-  /*public static void main(String[] args) {
-    System.out.println(CURRENT_VERSION);
-  }
-
-  private JUMPVersion() {}
-*/
-  /**
-   * Gets the major number of the release version.
-   *
-   * @return the major number of the release version.
-   */
-  //public int getMajor() { return MAJOR; }
-
-  /**
-   * Gets the minor number of the release version.
-   *
-   * @return the minor number of the release version.
-   */
-  //public int getMinor() { return MINOR; }
-
-  /**
-   * Gets the patch number of the release version.
-   *
-   * @return the patch number of the release version.
-   */
-  //public int getPatch() { return PATCH; }
+  public static final String CURRENT_VERSION = I18N
+      .get("JUMPWorkbench.version.number")
+      + " "
+      + I18N.get("JUMPWorkbench.version.release")
+      + " rev."
+      + I18N.get("JUMPWorkbench.version.revision");
 
   /**
    * Gets the full version number, suitable for display.
-   *
+   * 
    * @return the full version number, suitable for display.
    */
   public String toString() {
-    String ver = I18N.get("JUMPWorkbench.version.number");
-    String releaseInfo = I18N.get("JUMPWorkbench.version.release");
-    if (releaseInfo != null && releaseInfo.length() > 0)
-      return ver + " " + releaseInfo;
-    return ver;
+    return CURRENT_VERSION;
   }
 
 }
