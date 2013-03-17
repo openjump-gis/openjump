89e819fd79941ada0126d130fce0485c84311538
Edgar Soldin <edso@users.sourceforge.net>
2013-03-17 18:58:50 +0000

    detect ram size and set appropriate Xmx value on windows

--- scripts/oj_windows.bat
+++ scripts/oj_windows.bat
@@ -1,5 +1,7 @@
-@echo off
-rem -- Detect current dir and OJ home --
+@echo off & setlocal
+rem call :init
+
+rem -- Save current working dir and OJ home --
 set OLD_DIR=%CD%
 set JUMP_HOME=%~dp0..%
 
@@ -19,9 +21,11 @@ set JAVA_OPTS=
 rem --- uncomment and change your language/country here to overwrite OS locale setting ---
 rem set JAVA_OPTS=%JAVA_OPTS% -Duser.language=de -Duser.country=DE
 
-rem --- change your memory configuration here - Xms is initial size, Xmx is maximum size, ---
-rem --- values are ##M for ## Megabytes, ##G for ## Gigabytes ---
-set JAVA_OPTS=%JAVA_OPTS% -Xms64M -Xmx512M
+rem --- enforce a memory configuration here, default value is the ---
+rem --- size of ram or 1GB for 32bit jre's, whichever is bigger   ---
+rem --- Xms is initial size, Xmx is maximum size, values          ---
+rem --- are ##M for ## Megabytes, ##G for ## Gigabytes            ---
+rem set JAVA_MEM=-Xms64M -Xmx512M
 
 rem --- uncomment and change your http proxy settings here
 rem set JAVA_OPTS=%JAVA_OPTS% -Dhttp.proxyHost=myproxyserver.com -Dhttp.proxyPort=80 -Dhttp.noProxyHosts="localhost|host.mydomain.com"
@@ -63,7 +67,16 @@ rem -- find java runtime --
 rem -- show java version (for debugging) --
 for %%F in ("%JAVA%") do set "dirname=%%~dpF"
 echo Using '%JAVA_BIN%' found in '%dirname%'
-"%dirname%java" -version
+rem "%dirname%java" -version
+SET concat=
+for /f "tokens=* delims=" %%i in ('"%dirname%java" -version 2^>^&1') do call :concat "; " %%i
+set "JAVA_VERSIONSTRING=%concat%"
+echo %JAVA_VERSIONSTRING%
+
+rem -- detect if java is 64bit --
+for /f "delims=" %%v in ('echo "%JAVA_VERSIONSTRING%"^|findstr /I "64-Bit"') do (
+  set JAVA_X64=64
+)
 
 rem -- Change to jump home dir --
 rem -- NOTE: mount UNC paths to a local drive for this --
@@ -72,7 +85,7 @@ cd /D "%JUMP_HOME%"
 rem -- Uninstall if asked nicely ---
 if "%1"=="--uninstall" ( 
   "%JAVA%" -jar .\uninstall\uninstaller.jar
-  goto :eof
+  goto:eof
 )
 
 set LIB=lib
@@ -130,7 +143,7 @@ rem -- set settings home/log dir if none given --
   if NOT DEFINED SETTINGS_HOME (
     rem ---- check if jumphome is writable ----
     copy /Y NUL "%JUMP_HOME%\.writable" > NUL 2>&1 && set WRITEOK=1
-    IF DEFINED WRITEOK ( 
+    if DEFINED WRITEOK ( 
       rem ---- an absolute settings_home allows file:/// for log4j conf ----
       set "SETTINGS_HOME=%JUMP_HOME%"
      ) else (
@@ -151,8 +164,13 @@ if EXIST "%SETTINGS_HOME%\log4j.xml" (
   set LOG4J_CONF=.\bin\log4j.xml
 )
 
+rem -- add memory settings --
+if NOT DEFINED JAVA_MEM (
+  call :memory
+)
+
 rem -- essential options, don't change unless you know what you're doing --
-set JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration="%LOG4J_CONF%" -Dlog.dir="%LOG_DIR%" -Djump.home="%JUMP_HOME%"
+set JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration="%LOG4J_CONF%" -Dlog.dir="%LOG_DIR%" -Djump.home="%JUMP_HOME%" %JAVA_MEM%
 
 rem -- set default app options --
 set JUMP_OPTS=-default-plugins bin\default-plugins.xml -state "%SETTINGS_HOME%" -plug-in-directory "%LIB%\ext"
@@ -162,26 +180,99 @@ if EXIST "bin\workbench-properties.xml" set "JUMP_OPTS=%JUMP_OPTS% -properties b
 rem -- disconnect javaw from console by using start --
 rem -- note: title is needed or start won't accept quoted path to java binary (protect spaces in javapath) --
 if /i "%JAVA_BIN%"=="javaw" ( set START=start "" ) else ( set START= )
-
-%START% "%JAVA%" -cp "%CLASSPATH%" %JAVA_OPTS% com.vividsolutions.jump.workbench.JUMPWorkbench %JUMP_OPTS% %*
+if /i NOT "%JAVA_BIN%"=="javaw" echo ---Start OJ---
+ %START% "%JAVA%" -cp "%CLASSPATH%" %JAVA_OPTS% com.vividsolutions.jump.workbench.JUMPWorkbench %JUMP_OPTS% %*
 
 cd /D %OLD_DIR%
 
 rem -- give user a chance to see console output if we are in console mode but the app finished already
 if /i NOT "%JAVA_BIN%"=="javaw" pause
 
-goto :eof
+goto:eof
+
+:init
+rem --- reset variables ---
+set JAVA=
+set JAVA_MEM=
+set JAVA_BIN=
+set SETTINGS_HOME=
+goto:eof
 
 :setclass
 set CLASSPATH=%CLASSPATH%;%jarfile%
 set jarfile=
-goto :eof
+goto:eof
 
 :dequote
 SETLOCAL enabledelayedexpansion
 set string=%*
 if DEFINED string set string=!string:"=!
 ENDLOCAL & set unquoted=%string%
-goto :eof
+goto:eof
+
+:concat
+set "glue=%~1"
+shift
+set string=
+:loop
+set glue2=
+if DEFINED string set "glue2= "
+if NOT [%1]==[] (
+  set "string=%string%%glue2%%1"
+  shift
+  goto :loop
+)
+if NOT "%concat%" == "" (
+  set "string=%glue%%string%"
+)
+set "concat=%concat%%string%"
+goto:eof
+
+:memory
+if /i NOT "%JAVA_BIN%"=="javaw" echo ---Detect maximum memory limit---
+
+rem --- detect ram size, values are in kB ---
+for /f "delims=" %%l in ('wmic os get FreePhysicalMemory^,TotalVisibleMemorySize /format:list') do >nul 2>&1 set "OS_%%l"
+if NOT DEFINED OS_TotalVisibleMemorySize call :mem_failed Couldn't determine ram size.
+rem --- use 100% of ram as default limit (1.124 is a factor to make java64 really use that much) ---
+set /a "JAVA_XMX=%OS_TotalVisibleMemorySize%/1000*1124"
+set /a "JAVA_XMX_X86=1024*1024"
+set /a "JAVA_RAM_QUARTER=%OS_TotalVisibleMemorySize%/4"
+rem --- a. cap to 1GB for 32bit jre ---
+rem --- b. use xmx value if it fits into free space ---
+rem --- c. use freemem value if bigger than 1/4 ram (jre default) ---
+rem --- d. don't set, use jre default (works even though less than 1/4 ram might be free) ---
+
+if NOT DEFINED JAVA_X64 if %JAVA_XMX% GTR %JAVA_XMX_X86% goto :use_cap
+goto use_max
+rem if %OS_FreePhysicalMemory% GEQ %JAVA_XMX%
+rem if %OS_FreePhysicalMemory% GTR %JAVA_RAM_QUARTER% goto :use_free
+rem if /i NOT "%JAVA_BIN%"=="javaw" echo Less than 1/4 ram free. Use jre default.
+goto:eof
+
+:use_cap
+  rem --- if x86 cap is bigger than actual ram, use ram size instead ---
+  if %JAVA_XMX_X86% GTR %OS_TotalVisibleMemorySize% call :use_max
+  call :xmx %JAVA_XMX_X86%
+  if /i NOT "%JAVA_BIN%"=="javaw" echo set %JAVA_MEM_STRING% ^(32 bit jre maximum^)
+  goto:eof
+:use_max
+  call :xmx %JAVA_XMX%
+  if /i NOT "%JAVA_BIN%"=="javaw" echo set %JAVA_MEM_STRING% ^(ram maximum^)
+  goto:eof
+:use_free
+  call :xmx %OS_FreePhysicalMemory%
+  if /i NOT "%JAVA_BIN%"=="javaw" call echo set %JAVA_MEM_STRING% ^(free memory^)
+  goto:eof
+:mem_failed
+  if /i NOT "%JAVA_BIN%"=="javaw" call echo skipped because: %*
+  goto:eof
+
+:xmx
+set /a "value=%1/1024"
+set JAVA_MEM=-Xmx%value%M
+set "JAVA_MEM_STRING=Xmx to %value%M"
+goto:eof
+
 
 :eof
