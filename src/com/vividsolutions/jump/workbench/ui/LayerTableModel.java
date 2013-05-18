2a279fe4d5fd658314d8cf9eafb4cf2c1efae76b
Michael Michaud <michaudm@users.sourceforge.net>
2013-05-18 18:24:26 +0000

    fix bug 3613234 : sorting attributes did not respect comparator contract

--- src/com/vividsolutions/jump/workbench/ui/LayerTableModel.java
+++ src/com/vividsolutions/jump/workbench/ui/LayerTableModel.java
@@ -369,6 +369,7 @@ public class LayerTableModel extends ColumnBasedTableModel {
 
     private static int compareValue(Object o1, Object o2)
     {
+      if (o1 == null && o2 == null) return 0;
       if (o1 == null) return -1;
       if (o2 == null) return 1;
       
