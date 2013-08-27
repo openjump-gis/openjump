a18f7a7232b5c69603cc1cb4884f0e8d16eb5473
Michael Michaud <michaudm@users.sourceforge.net>
2013-08-27 20:59:30 +0000

    ticket #330 : workaround for java 7 / IPv6 compatibility problem

--- src/com/vividsolutions/jump/datastore/postgis/PostgisDataStoreDriver.java
+++ src/com/vividsolutions/jump/datastore/postgis/PostgisDataStoreDriver.java
@@ -72,7 +72,14 @@ public class PostgisDataStoreDriver
     Driver driver = (Driver) Class.forName(JDBC_CLASS).newInstance();
     DriverManager.registerDriver(driver);
 
+    // mmichaud 2013-08-27 workaround for ticket #330
+    String savePreferIPv4Stack = System.getProperty("java.net.preferIPv4Stack");
+    String savePreferIPv6Addresses = System.getProperty("java.net.preferIPv6Addresses");
+    System.setProperty("java.net.preferIPv4Stack", "true");
+    System.setProperty("java.net.preferIPv6Addresses", "false");
     Connection conn = DriverManager.getConnection(url, user, password);
+    System.setProperty("java.net.preferIPv4Stack", savePreferIPv4Stack);
+    System.setProperty("java.net.preferIPv6Addresses", savePreferIPv6Addresses);
     return new PostgisDSConnection(conn);
   }
   public boolean isAdHocQuerySupported() {
