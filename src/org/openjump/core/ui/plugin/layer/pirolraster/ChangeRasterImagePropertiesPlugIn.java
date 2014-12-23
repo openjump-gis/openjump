da44acfe85edaabc2fcbec42dee821e8afc3500d
Giuseppe Aruta <ma15569@users.sourceforge.net>
2014-12-23 13:49:23 +0000

    [Giuseppe Aruta Dic 23 2014] Transformed into MultiImput Dialog. Added Raster Transparency, ScaleStyle and ColorEditor

--- src/org/openjump/core/ui/plugin/layer/pirolraster/ChangeRasterImagePropertiesPlugIn.java
+++ src/org/openjump/core/ui/plugin/layer/pirolraster/ChangeRasterImagePropertiesPlugIn.java
@@ -1,90 +1,168 @@
-/*
- * Created on 03.07.2005
- *
- * CVS information:
- *  $Author: LBST-PF-3\orahn $
- *  $Date: 2006-10-06 10:01:50 +0000 (Fr, 06 Okt 2006) $
- *  $ID$
- *  $Rev: 2509 $
- *  $Id: ChangeRasterImagePropertiesPlugIn.java 2509 2006-10-06 10:01:50Z LBST-PF-3\orahn $
- *  $Log: ChangeRasterImagePropertiesPlugIn.java,v $
- *
- */
+
 package org.openjump.core.ui.plugin.layer.pirolraster;
+
+
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import org.openjump.core.apitools.LayerTools;
 import org.openjump.core.rasterimage.RasterImageLayer;
+import org.openjump.core.ui.plugin.layer.pirolraster.panel.RasterColorEditorPanel;
+import org.openjump.core.ui.plugin.layer.pirolraster.panel.RasterTransparencyPanel;
+import org.openjump.core.ui.plugin.layer.pirolraster.panel.RasterScaleStylePanel;
 
+
+import com.vividsolutions.jts.util.Assert;
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
 import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
+import com.vividsolutions.jump.workbench.plugin.EnableCheck;
 import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
 import com.vividsolutions.jump.workbench.plugin.MultiEnableCheck;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.ui.GUIUtil;
+import com.vividsolutions.jump.workbench.ui.MultiInputDialog;
+import com.vividsolutions.jump.workbench.ui.images.IconLoader;
+import com.vividsolutions.jump.workbench.ui.style.StylePanel;
+
 
 /**
- * 
- * PlugIn that adds a menu item to the layer context menu, that enables changing RasterImage layer
- * transparencies add any time.
- *
- * @author Ole Rahn
- * <br>
- * <br>FH Osnabr&uuml;ck - University of Applied Sciences Osnabr&uuml;ck,
- * <br>Project: PIROL (2005),
- * <br>Subproject: Daten- und Wissensmanagement
- * 
- * @version $Rev: 2509 $
- * [sstein] - 22.Feb.2009 - modified to work in OpenJUMP
+ * Dic 23 2014
+ * [Giuseppe Aruta] - Transformed in a MultiImputDialog. 
+ * Add Added Raster Transparency, ScaleStyle and ColorEditor Panels
  */
+@SuppressWarnings("deprecation")
 public class ChangeRasterImagePropertiesPlugIn extends AbstractPlugIn {
+	 private final static String LAST_TAB_KEY = ChangeRasterImagePropertiesPlugIn.class.getName() +
+		        " - LAST TAB";
 
-    public ChangeRasterImagePropertiesPlugIn(){
-        //super(new PersonalLogger(DebugUserIds.OLE));
-    }
-    
-    /**
-     * @inheritDoc
-     */
-    public String getIconString() {
-        return null;
-    }
+public ChangeRasterImagePropertiesPlugIn() {
+		    }
+		   
+		/* Deactivated     
+			public void initialize(PlugInContext context) throws Exception
+		    {    
+		      WorkbenchContext workbenchContext = context.getWorkbenchContext();
+		       new FeatureInstaller(workbenchContext);
+		        context.getFeatureInstaller().addMainMenuPlugin(
+		        		this, 
+		        		new String[] {MenuNames.RASTER},
+		        		getName(),
+		        		false, 
+						getIcon(),
+						createEnableCheck(context.getWorkbenchContext()));
+				}
+          */
+			public boolean execute(PlugInContext context) throws Exception{
+					final RasterImageLayer rLayer = (RasterImageLayer)LayerTools.getSelectedLayerable(context, RasterImageLayer.class);
+					final MultiInputDialog dialog = new MultiInputDialog(context.getWorkbenchFrame(),
+		                I18N.get("org.openjump.core.ui.plugin.layer.pirolraster.ChangeRasterImagePropertiesPlugIn.Change-Raster-Image-Properties"), true);
+		       
+					dialog.setSize(500,400);
+					dialog.setInset(0);
+					dialog.setApplyVisible(true);
+		     
+					final ArrayList<JPanel> stylePanels = new ArrayList();
+					final RasterScaleStylePanel rasterScalepanel = new RasterScaleStylePanel(rLayer, context.getLayerViewPanel());
+					final RasterTransparencyPanel rasstyle = new RasterTransparencyPanel(rLayer);
+					final RasterColorEditorPanel rascolorpanel = new RasterColorEditorPanel(context, rLayer);
+		       
+					stylePanels.add(rasstyle);   
+					stylePanels.add(rasterScalepanel);  
+					stylePanels.add(rascolorpanel ); 
+		        
+					JTabbedPane tabbedPane = new JTabbedPane();
 
-    /**
-     * @inheritDoc
-     */
-    public String getName() {
-        return I18N.get("org.openjump.core.ui.plugin.layer.pirolraster.ChangeRasterImagePropertiesPlugIn.Change-Raster-Image-Properties");
-    }
-    
-    /**
-     *@inheritDoc
-     */
-    public boolean execute(PlugInContext context) throws Exception {
-        RasterImageLayer rlayer = (RasterImageLayer)LayerTools.getSelectedLayerable(context, RasterImageLayer.class);
-        
-        if (rlayer==null){
-            context.getWorkbenchFrame().warnUser(I18N.get("pirol.plugIns.EditAttributeByFormulaPlugIn.no-layer-selected"));
-            return false;
-        } 
-        
-        ChangeRasterImageStyleDialog dialog = new ChangeRasterImageStyleDialog(rlayer, context.getWorkbenchFrame(), this.getName(), true);
-        
-        dialog.setVisible(true);
-        
-        if (!dialog.wasOkClicked())
-            return false;
+					for (Iterator i = stylePanels.iterator(); i.hasNext();) {
+						final StylePanel stylePanel = (StylePanel) i.next();
+						tabbedPane.add((Component) stylePanel, stylePanel.getTitle());
+						dialog.addEnableChecks(stylePanel.getTitle(),
+		                Arrays.asList(
+		                    new EnableCheck[] {
+		                        new EnableCheck() {
+		                        public String check(JComponent component) {
+		                            return stylePanel.validateInput();
+		                        }
+		                    }
+		                    }));
+		        } 
 
-        rlayer.fireAppearanceChanged();
-        
-        return true;
-    }
-    
-    public static MultiEnableCheck createEnableCheck(final WorkbenchContext workbenchContext) {
-        EnableCheckFactory checkFactory = new EnableCheckFactory(workbenchContext);      
-        return new MultiEnableCheck()
-        .add(checkFactory.createExactlyNLayerablesMustBeSelectedCheck(1, RasterImageLayer.class));
-	}
-    
-    public void initialize(PlugInContext context) throws Exception {}
+					dialog.addRow(tabbedPane);
+					tabbedPane.setSelectedComponent(find(stylePanels,
+		                (String) context.getWorkbenchContext().getWorkbench()
+		                                .getBlackboard().get(LAST_TAB_KEY,
+		                    ((StylePanel) stylePanels.iterator().next()).getTitle())));
+		        
+		            context.getWorkbenchContext().getWorkbench().getBlackboard().put(LAST_TAB_KEY,
+		            		((StylePanel) tabbedPane.getSelectedComponent()).getTitle());
 
+		            dialog.addOKCancelApplyPanelActionListener(new ActionListener(){
+		            	public void actionPerformed(ActionEvent e) {
+		            		if (dialog.wasApplyPressed()) {
+		                    rasterScalepanel.updateStyles();                
+		                	rasstyle.updateStyles();
+		                	rascolorpanel.updateStyles();
+		            		}
+		            	}
+		            });
+		        
+		            dialog.pack();
+		            GUIUtil.centreOnWindow(dialog);
+		            dialog.setVisible(true);
+		        
+		            if (dialog.wasOKPressed()) {
+		            	rasterScalepanel.updateStyles();                
+		            	rasstyle.updateStyles(); 
+		            	rascolorpanel .updateStyles(); 
+		            	return true;
+		            }
+		            return false;
+		    	}
+ 
+		    private Component find(Collection stylePanels, String title) {
+		        for (Iterator i = stylePanels.iterator(); i.hasNext();) {
+		            StylePanel stylePanel = (StylePanel) i.next();
+
+		            if (stylePanel.getTitle().equals(title)) {
+		                return (Component) stylePanel;
+		            }
+		        }
+
+		        Assert.shouldNeverReachHere();
+
+		        return null;
+		    }
+
+		    public String getName() {
+		        return  I18N.get("org.openjump.core.ui.plugin.layer.pirolraster.ChangeRasterImagePropertiesPlugIn.Change-Raster-Image-Properties"); 
+						}
+		    
+		    public ImageIcon getIcon() {
+		        return  GUIUtil.toSmallIcon(IconLoader.icon("color_wheel.png")); 
+		    }
+		    
+		   
+			 public static MultiEnableCheck createEnableCheck(
+						WorkbenchContext workbenchContext) {
+				
+					EnableCheckFactory checkFactory = new EnableCheckFactory(
+							workbenchContext);
+					MultiEnableCheck multiEnableCheck = new MultiEnableCheck();
+				multiEnableCheck.add(checkFactory.createExactlyNLayerablesMustBeSelectedCheck(1,RasterImageLayer.class));
+					return multiEnableCheck;
+				}
+			 
+			
+
+			 
 }
