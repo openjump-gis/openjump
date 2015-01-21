a64bb6b6ff6870b1892ec2d7fc292e3e75d03fde
Giuseppe Aruta <ma15569@users.sourceforge.net>
2015-01-21 16:13:29 +0000

    Delete obsolete icons

--- src/org/openjump/core/ui/plugin/layer/ExportLayerableEnvelopeAsGeometryPlugIn.java
+++ src/org/openjump/core/ui/plugin/layer/ExportLayerableEnvelopeAsGeometryPlugIn.java
@@ -48,10 +48,14 @@ import de.latlon.deejump.wfs.jump.WFSLayer;
 /**
  * Giuseppe Aruta 2015_01_15
  * 
- * PlugIn to export the envelope of any type of layer (Layer.class,
+ * PlugIn to export the envelope of any type of layerable (Layer.class,
  * ReferencedImageStyle.class, WMSLayer.class, WFSLayer.class,
  * RasterImageLayer.class) as geometry. Layer name, layer datasource (if any)
  * and CRS (only for WMS and WFS) are saved as layer attributes
+ * 
+ * Giuseppe Aruta 2015_01_19 If multiple layerables are selected, It will be
+ * exported one geometry including all of them
+ * 
  */
 
 public class ExportLayerableEnvelopeAsGeometryPlugIn extends AbstractPlugIn {
@@ -60,6 +64,8 @@ public class ExportLayerableEnvelopeAsGeometryPlugIn extends AbstractPlugIn {
             .get("org.openjump.core.ui.plugin.layer.LayerPropertiesPlugIn.Source-Path");
     private final static String NOTSAVED = I18N
             .get("org.openjump.core.ui.plugin.layer.LayerPropertiesPlugIn.Not-Saved");
+    private final static String MULTIPLESOURCE = I18N
+            .get("org.openjump.core.ui.plugin.layer.LayerPropertiesPlugIn.Multiple-Sources");
 
     private final static String LAYER = I18N.get("ui.GenericNames.LAYER");
 
@@ -84,7 +90,7 @@ public class ExportLayerableEnvelopeAsGeometryPlugIn extends AbstractPlugIn {
 
         return new MultiEnableCheck().add(
                 checkFactory.createWindowWithLayerNamePanelMustBeActiveCheck())
-                .add(checkFactory.createExactlyNLayerablesMustBeSelectedCheck(
+                .add(checkFactory.createAtLeastNLayerablesMustBeSelectedCheck(
                         1, Layerable.class));
     }
 
@@ -132,37 +138,54 @@ public class ExportLayerableEnvelopeAsGeometryPlugIn extends AbstractPlugIn {
     public boolean execute(PlugInContext context) throws Exception {
         Layerable layer = (Layerable) LayerTools.getSelectedLayerable(context,
                 Layerable.class);
-        Envelope e = new Envelope();
-        String name = layer.getName();
+        final WorkbenchContext wbcontext = context.getWorkbenchContext();
+        Envelope envelope = new Envelope();
+        int size = -1;// Layer size
+        size = layer.getLayerManager().size();// Get number
+        for (Iterator i = wbcontext.getLayerNamePanel()
+                .selectedNodes(Layerable.class).iterator(); i.hasNext();) {
+            Layerable slayer = (Layerable) i.next();
+            if (slayer instanceof WMSLayer) {
+                envelope.expandToInclude(((WMSLayer) slayer).getEnvelope());
+            } else if (slayer instanceof WFSLayer) {
+                envelope.expandToInclude(((WFSLayer) slayer)
+                        .getFeatureCollectionWrapper().getEnvelope());
+
+            } else if (slayer instanceof Layer) {
+                if (((Layer) slayer).getFeatureCollectionWrapper().isEmpty()) {
+                    context.getWorkbenchFrame()
+                            .warnUser(
+                                    I18N.get("org.openjump.sigle.plugin.ReplaceValuePlugIn.Layer-has-no-feature"));
+                    return false;
+                } else {
+                    envelope.expandToInclude(((Layer) slayer)
+                            .getFeatureCollectionWrapper().getEnvelope());
+                }
+
+            } else if (slayer instanceof RasterImageLayer) {
+                envelope.expandToInclude(((RasterImageLayer) slayer)
+                        .getWholeImageEnvelope());
+            }
+
+        }
+
+        String name = null;
         String sourceClass = "";
         String sourcePath = NOTSAVED;
 
-        if (layer instanceof WMSLayer) {
-            e = ((WMSLayer) layer).getEnvelope();
-
-        } else if (layer instanceof WFSLayer) {
-            e = ((WFSLayer) layer).getFeatureCollectionWrapper().getEnvelope();
-
-        } else if (layer instanceof Layer) {
-            if (((Layer) layer).getFeatureCollectionWrapper().isEmpty()) {
-                context.getWorkbenchFrame()
-                        .warnUser(
-                                I18N.get("org.openjump.sigle.plugin.ReplaceValuePlugIn.Layer-has-no-feature"));
-                return false;
-            } else {
-                e = ((Layer) layer).getFeatureCollectionWrapper().getEnvelope();
-            }
-
-        } else if (layer instanceof RasterImageLayer) {
-            e = ((RasterImageLayer) layer).getWholeImageEnvelope();
-        }
-        Geometry geom = new GeometryFactory().createPolygon(
-                new GeometryFactory().createLinearRing(new Coordinate[] {
-                        new Coordinate(e.getMinX(), e.getMinY()),
-                        new Coordinate(e.getMinX(), e.getMaxY()),
-                        new Coordinate(e.getMaxX(), e.getMaxY()),
-                        new Coordinate(e.getMaxX(), e.getMinY()),
-                        new Coordinate(e.getMinX(), e.getMinY()) }), null);
+        Geometry geom = new GeometryFactory()
+                .createPolygon(new GeometryFactory()
+                        .createLinearRing(new Coordinate[] {
+                                new Coordinate(envelope.getMinX(), envelope
+                                        .getMinY()),
+                                new Coordinate(envelope.getMinX(), envelope
+                                        .getMaxY()),
+                                new Coordinate(envelope.getMaxX(), envelope
+                                        .getMaxY()),
+                                new Coordinate(envelope.getMaxX(), envelope
+                                        .getMinY()),
+                                new Coordinate(envelope.getMinX(), envelope
+                                        .getMinY()) }), null);
 
         FeatureCollection newFeaturecollection = new FeatureDataset(
                 (FeatureSchema) ExportLayerableEnvelopeAsGeometryPlugIn.defaultSchema
@@ -173,57 +196,70 @@ public class ExportLayerableEnvelopeAsGeometryPlugIn extends AbstractPlugIn {
                         .clone());
 
         feature.setAttribute("GEOMETRY", geom);
-        feature.setAttribute(LAYER, name);
-        if (layer instanceof WMSLayer) {
-            feature.setAttribute(SOURCE_PATH, ((WMSLayer) layer).getServerURL());
-            feature.setAttribute("CRS", ((WMSLayer) layer).getSRS());
-        } else if (layer instanceof WFSLayer) {
-            feature.setAttribute(SOURCE_PATH, ((WFSLayer) layer).getServerURL());
-            feature.setAttribute("CRS", ((WFSLayer) layer).getCrs());
-        } else if (layer instanceof RasterImageLayer) {
-            sourcePath = ((RasterImageLayer) layer).getImageFileName();
-            feature.setAttribute(SOURCE_PATH, sourcePath);
-            feature.setAttribute("CRS", null);
 
-        } else if (layer instanceof Layer
-                && ((Layer) layer).getStyle(ReferencedImageStyle.class) == null) {
+        if (size == 1) {
+            name = layer.getName();
+            feature.setAttribute(LAYER, name);
+            if (layer instanceof WMSLayer) {
+                feature.setAttribute(SOURCE_PATH,
+                        ((WMSLayer) layer).getServerURL());
 
-            DataSourceQuery dsq = ((Layer) layer).getDataSourceQuery();
-            if (dsq != null) {
-                String dsqSourceClass = dsq.getDataSource().getClass()
-                        .getName();
-                if (sourceClass.equals(""))
-                    sourceClass = dsqSourceClass;
-                Object fnameObj = dsq.getDataSource().getProperties()
-                        .get("File");
-                if (fnameObj == null) {
-                    fnameObj = dsq.getDataSource().getProperties()
-                            .get(DataStoreDataSource.CONNECTION_DESCRIPTOR_KEY);
+                feature.setAttribute("CRS", ((WMSLayer) layer).getSRS());
+            } else if (layer instanceof WFSLayer) {
+                feature.setAttribute(SOURCE_PATH,
+                        ((WFSLayer) layer).getServerURL());
+                feature.setAttribute("CRS", ((WFSLayer) layer).getCrs());
+            } else if (layer instanceof RasterImageLayer) {
+                sourcePath = ((RasterImageLayer) layer).getImageFileName();
+                feature.setAttribute(SOURCE_PATH, sourcePath);
+                feature.setAttribute("CRS", null);
+
+            } else if (layer instanceof Layer
+                    && ((Layer) layer).getStyle(ReferencedImageStyle.class) == null) {
+
+                DataSourceQuery dsq = ((Layer) layer).getDataSourceQuery();
+                if (dsq != null) {
+                    String dsqSourceClass = dsq.getDataSource().getClass()
+                            .getName();
+                    if (sourceClass.equals(""))
+                        sourceClass = dsqSourceClass;
+                    Object fnameObj = dsq.getDataSource().getProperties()
+                            .get("File");
+                    if (fnameObj == null) {
+                        fnameObj = dsq
+                                .getDataSource()
+                                .getProperties()
+                                .get(DataStoreDataSource.CONNECTION_DESCRIPTOR_KEY);
+                    }
+                    if (fnameObj != null) {
+                        sourcePath = fnameObj.toString();
+                    }
                 }
-                if (fnameObj != null) {
-                    sourcePath = fnameObj.toString();
+
+                feature.setAttribute(SOURCE_PATH, sourcePath);
+                feature.setAttribute("CRS", null);
+
+            } else if (layer instanceof Layer
+                    && ((Layer) layer).getStyle(ReferencedImageStyle.class) != null) {
+
+                FeatureCollection featureCollection = ((Layer) layer)
+                        .getFeatureCollectionWrapper();
+                for (Iterator i = featureCollection.iterator(); i.hasNext();) {
+                    Feature feat = (Feature) i.next();
+                    sourcePath = (String) feat.getString("IMG_URI");
+                    sourcePath = sourcePath.substring(5);
+
                 }
+
+                feature.setAttribute(SOURCE_PATH, sourcePath);
+                feature.setAttribute("CRS", null);
             }
-
-            feature.setAttribute(SOURCE_PATH, sourcePath);
-            feature.setAttribute("CRS", null);
-
-        } else if (layer instanceof Layer
-                && ((Layer) layer).getStyle(ReferencedImageStyle.class) != null) {
-
-            FeatureCollection featureCollection = ((Layer) layer)
-                    .getFeatureCollectionWrapper();
-            for (Iterator i = featureCollection.iterator(); i.hasNext();) {
-                Feature feat = (Feature) i.next();
-                sourcePath = (String) feat.getString("IMG_URI");
-                sourcePath = sourcePath.substring(5);
-
-            }
-
-            feature.setAttribute(SOURCE_PATH, sourcePath);
+        } else {
+            name = MULTIPLESOURCE;
+            feature.setAttribute(LAYER, name);
+            feature.setAttribute(SOURCE_PATH, MULTIPLESOURCE);
             feature.setAttribute("CRS", null);
         }
-
         newFeaturecollection.add(feature);
 
         addLayer(
