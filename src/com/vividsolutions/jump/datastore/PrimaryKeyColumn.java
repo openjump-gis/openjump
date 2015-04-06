fd6ca134de029cb9e3966206526df0914753105f
Michael Michaud <michaudm@users.sourceforge.net>
2015-04-06 08:44:09 +0000

    replace startsWith by equals to convert postgresql DataType names to jdbc data types

--- src/com/vividsolutions/jump/datastore/PrimaryKeyColumn.java
+++ src/com/vividsolutions/jump/datastore/PrimaryKeyColumn.java
@@ -18,20 +18,26 @@ public class PrimaryKeyColumn {
     public PrimaryKeyColumn(String name, String sqlTypeName) {
         this(name);
         this.sqlTypeName = sqlTypeName;
-        if (sqlTypeName.startsWith("character")) this.sqlType = Types.VARCHAR;
-        else if (sqlTypeName.startsWith("text")) this.sqlType = Types.VARCHAR;
-        else if (sqlTypeName.startsWith("integer")) this.sqlType = Types.INTEGER;
-        else if (sqlTypeName.startsWith("big")) this.sqlType = Types.BIGINT;
-        else if (sqlTypeName.startsWith("bit")) this.sqlType = Types.BIT;
-        else if (sqlTypeName.startsWith("boolean")) this.sqlType = Types.BOOLEAN;
-        else if (sqlTypeName.startsWith("date")) this.sqlType = Types.DATE;
-        else if (sqlTypeName.startsWith("double")) this.sqlType = Types.DOUBLE;
-        else if (sqlTypeName.startsWith("numeric")) this.sqlType = Types.NUMERIC;
-        else if (sqlTypeName.startsWith("real")) this.sqlType = Types.REAL;
-        else if (sqlTypeName.startsWith("smallint")) this.sqlType = Types.SMALLINT;
-        else if (sqlTypeName.startsWith("serial")) this.sqlType = Types.BIGINT;
-        else if (sqlTypeName.startsWith("timestamp")) this.sqlType = Types.TIMESTAMP;
-        else if (sqlTypeName.startsWith("time")) this.sqlType = Types.TIME;
+        if (sqlTypeName.equals("character")) this.sqlType = Types.VARCHAR;
+        else if (sqlTypeName.equals("character varying")) this.sqlType = Types.VARCHAR;
+        else if (sqlTypeName.equals("text")) this.sqlType = Types.VARCHAR;
+        else if (sqlTypeName.equals("integer")) this.sqlType = Types.INTEGER;
+        else if (sqlTypeName.equals("bigint")) this.sqlType = Types.BIGINT;
+        else if (sqlTypeName.equals("bigserial")) this.sqlType = Types.BIGINT;
+        else if (sqlTypeName.equals("bit")) this.sqlType = Types.BIT;
+        else if (sqlTypeName.equals("boolean")) this.sqlType = Types.BOOLEAN;
+        else if (sqlTypeName.equals("date")) this.sqlType = Types.DATE;
+        else if (sqlTypeName.equals("double")) this.sqlType = Types.DOUBLE;
+        else if (sqlTypeName.equals("double precision")) this.sqlType = Types.DOUBLE;
+        else if (sqlTypeName.equals("json")) this.sqlType = Types.VARCHAR;
+        else if (sqlTypeName.equals("numeric")) this.sqlType = Types.NUMERIC;
+        else if (sqlTypeName.equals("real")) this.sqlType = Types.REAL;
+        else if (sqlTypeName.equals("smallint")) this.sqlType = Types.SMALLINT;
+        else if (sqlTypeName.equals("serial")) this.sqlType = Types.BIGINT;
+        else if (sqlTypeName.equals("timestamp")) this.sqlType = Types.TIMESTAMP;
+        else if (sqlTypeName.equals("timestamp with time zone")) this.sqlType = Types.TIMESTAMP;
+        else if (sqlTypeName.equals("timestamp without time zone")) this.sqlType = Types.TIMESTAMP;
+        else if (sqlTypeName.equals("time")) this.sqlType = Types.TIME;
         else this.sqlType = Types.JAVA_OBJECT;
     }
 
