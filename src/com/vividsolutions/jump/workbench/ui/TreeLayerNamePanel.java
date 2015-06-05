f3cb42f43d0ae3afc187ba86a6add2d449324f39
Edgar Soldin <edso@users.sourceforge.net>
2015-06-05 08:28:53 +0000

    remove spurious import

--- src/com/vividsolutions/jump/workbench/ui/TreeLayerNamePanel.java
+++ src/com/vividsolutions/jump/workbench/ui/TreeLayerNamePanel.java
@@ -83,7 +83,6 @@ import com.vividsolutions.jump.workbench.model.Layerable;
 import com.vividsolutions.jump.workbench.model.WMSLayer;
 import com.vividsolutions.jump.workbench.ui.renderer.RenderingManager;
 import com.vividsolutions.jump.workbench.ui.renderer.style.BasicStyle;
-import org.stringtree.util.tree.Tree;
 
 public class TreeLayerNamePanel extends JPanel implements LayerListener,
     LayerNamePanel, LayerableNamePanel, LayerNamePanelProxy, PopupNodeProxy {
