af426152274a45e401bee854ece1117447222a7b
Giuseppe Aruta <ma15569@users.sourceforge.net>
2015-06-08 06:29:09 +0000

    implemented the dialogs as detachable internal frame

--- src/org/openjump/core/ui/plugin/wms/WMSLegendPlugIn.java
+++ src/org/openjump/core/ui/plugin/wms/WMSLegendPlugIn.java
@@ -13,13 +13,14 @@ import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import org.apache.commons.lang.StringUtils;
 import org.openjump.core.apitools.LayerTools;
+import org.openjump.core.ui.swing.DetachableInternalFrame;
 
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
@@ -28,6 +29,7 @@ import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
 import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
 import com.vividsolutions.jump.workbench.plugin.MultiEnableCheck;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.ui.images.IconLoader;
 import com.vividsolutions.wms.AbstractBasicRequest;
 import com.vividsolutions.wms.MapLayer;
 import com.vividsolutions.wms.WMService;
@@ -42,79 +44,77 @@ public class WMSLegendPlugIn extends AbstractPlugIn {
         super();
     }
 
+    private static ImageIcon ICON = IconLoader.icon("globe3_13.png");
+    private JScrollPane scrollPane = new JScrollPane();
+
     public boolean execute(final PlugInContext context) throws Exception {
+        final WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(
+                context, WMSLayer.class);
 
-        JDialog dialogLegend = new JDialog(context.getWorkbenchFrame(), PANEL,
-                false);
-        dialogLegend.setSize(200, 500);
-        JScrollPane sp = new JScrollPane(getLegendPanel(context));
+        DetachableInternalFrame frame = new DetachableInternalFrame();
+        frame.setTitle(PANEL + " (" + layer.getName() + ")");
+        frame.setResizable(true);
+        frame.setClosable(true);
+        frame.setIconifiable(true);
+        frame.setMaximizable(true);
+        frame.setFrameIcon(ICON);
+        frame.setSize(200, 500);
+        frame.setLayer(JLayeredPane.PALETTE_LAYER);
+        scrollPane = new JScrollPane(getLegendPanel(context));
+        frame.add(scrollPane, BorderLayout.CENTER);
 
-        dialogLegend.add(sp);
-
-        /*
-         * JPanel savePanel = new JPanel(); JButton saveButton = new JButton(
-         * I18N.get("deejump.plugin.SaveLegendPlugIn.Save-legend"));
-         * savePanel.add(saveButton);
-         * 
-         * saveButton.setAction(new SaveImageAsAction(dialogLegend,
-         * getLegendPanel(context)));// getLegendPanel(context)));
-         * 
-         * 
-         * dialogLegend.add(savePanel, BorderLayout.SOUTH);
-         */
-
-        dialogLegend.setVisible(true);
-        dialogLegend.validate();
+        context.getWorkbenchFrame().addInternalFrame(frame, true, true);
 
         return true;
     }
 
-//    // Get the WMS legend URL by name of layer
-//    private String getLegendUrl(PlugInContext context, String names)
-//            throws IOException {
-//        WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(context,
-//                WMSLayer.class);
-//        String serverURL = layer.getService().getServerUrl();
-//
-//        String version = layer.getWmsVersion();
-//        if ("1.0.0".equals(version)) {
-//            serverURL = serverURL
-//                    + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
-//        } else if (("1.1.0".equals(version)) || ("1.1.1".equals(version))
-//                || ("1.3.0".equals(version))) {
-//            serverURL = serverURL
-//                    + "&SERVICE=WMS&REQUEST=GetLegendGraphic&VERSION="
-//                    + version;
-//        }
-//        serverURL = serverURL + "&FORMAT=image/png&WIDTH=16&HEIGHT=16";
-//        serverURL = serverURL
-//                + "&legend_options=bgColor:0xFFFFEE;dpi:100;fontAntiAliasing:true;forceLabels:on";
-//
-//        serverURL = serverURL + "&LAYER=" + names;
-//        return serverURL;
-//
-//    }
+    // // Get the WMS legend URL by name of layer
+    // private String getLegendUrl(PlugInContext context, String names)
+    // throws IOException {
+    // WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(context,
+    // WMSLayer.class);
+    // String serverURL = layer.getService().getServerUrl();
+    //
+    // String version = layer.getWmsVersion();
+    // if ("1.0.0".equals(version)) {
+    // serverURL = serverURL
+    // + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
+    // } else if (("1.1.0".equals(version)) || ("1.1.1".equals(version))
+    // || ("1.3.0".equals(version))) {
+    // serverURL = serverURL
+    // + "&SERVICE=WMS&REQUEST=GetLegendGraphic&VERSION="
+    // + version;
+    // }
+    // serverURL = serverURL + "&FORMAT=image/png&WIDTH=16&HEIGHT=16";
+    // serverURL = serverURL
+    // +
+    // "&legend_options=bgColor:0xFFFFEE;dpi:100;fontAntiAliasing:true;forceLabels:on";
+    //
+    // serverURL = serverURL + "&LAYER=" + names;
+    // return serverURL;
+    //
+    // }
 
-//    // Get the WMS style URL by name of layer
-//    private String getStyles(PlugInContext context, String names)
-//            throws IOException {
-//        WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(context,
-//                WMSLayer.class);
-//        String serverURL = layer.getService().getServerUrl();
-//
-//        String version = layer.getWmsVersion();
-//        if ("1.0.0".equals(version)) {
-//            serverURL = serverURL
-//                    + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
-//        } else if (("1.1.0".equals(version)) || ("1.1.1".equals(version))
-//                || ("1.3.0".equals(version))) {
-//            serverURL = serverURL + "&SERVICE=WMS&REQUEST=GetStyles&VERSION="
-//                    + version;
-//        }
-//        serverURL = serverURL + "&LAYER=" + names;
-//        return serverURL;
-//
-//    }
+    // // Get the WMS style URL by name of layer
+    // private String getStyles(PlugInContext context, String names)
+    // throws IOException {
+    // WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(context,
+    // WMSLayer.class);
+    // String serverURL = layer.getService().getServerUrl();
+    //
+    // String version = layer.getWmsVersion();
+    // if ("1.0.0".equals(version)) {
+    // serverURL = serverURL
+    // + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
+    // } else if (("1.1.0".equals(version)) || ("1.1.1".equals(version))
+    // || ("1.3.0".equals(version))) {
+    // serverURL = serverURL + "&SERVICE=WMS&REQUEST=GetStyles&VERSION="
+    // + version;
+    // }
+    // serverURL = serverURL + "&LAYER=" + names;
+    // return serverURL;
+    //
+    // }
 
     // Modified From Kosmo SAIG. Build the legend panel. For each layer it
     // displays the layer name at the top and the legend at the bottom
@@ -125,7 +125,6 @@ public class WMSLegendPlugIn extends AbstractPlugIn {
         WMSLayer layer = (WMSLayer) LayerTools.getSelectedLayerable(context,
                 WMSLayer.class);
         List<String> names = layer.getLayerNames();
-
         mainPanel.setBackground(Color.WHITE);
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
@@ -143,18 +142,18 @@ public class WMSLegendPlugIn extends AbstractPlugIn {
             nameLabel.setHorizontalAlignment(JLabel.CENTER);
 
             nameLabel.setFont(new Font("Verdana", Font.BOLD, 16));
-            mainPanel.add(nameLabel, BorderLayout.NORTH);
+            mainPanel.add(nameLabel, BorderLayout.CENTER);
 
-//            String legendUrl = getLegendUrl(context, layerName);
-//            Image image;
-//            URL selectedUrl = null;
-//            selectedUrl = new URL(legendUrl);
-//            image = ImageIO.read(selectedUrl);
+            // String legendUrl = getLegendUrl(context, layerName);
+            // Image image;
+            // URL selectedUrl = null;
+            // selectedUrl = new URL(legendUrl);
+            // image = ImageIO.read(selectedUrl);
             LegendRequest req = new LegendRequest(layer.getService(), layerName);
             Image image = req.getImage();
             ImageIcon legendIcon = new ImageIcon(image);
 
-            if (/*getStyles(context, layerName) != null &&*/ legendIcon != null) {
+            if (/* getStyles(context, layerName) != null && */legendIcon != null) {
                 JLabel labelIcon = new JLabel(legendIcon, JLabel.CENTER);
                 mainPanel.add(labelIcon, BorderLayout.SOUTH);
             } else {
@@ -189,31 +188,32 @@ public class WMSLegendPlugIn extends AbstractPlugIn {
 }
 
 class LegendRequest extends AbstractBasicRequest {
-  private String layerName;
+    private String layerName;
 
-  public LegendRequest(WMService service, String name) {
-    super(service);
-    layerName = name;
-  }
-
-  public URL getURL() throws MalformedURLException {
-    String serverURL = service.getServerUrl();
-
-    String version = service.getVersion();
-    if (WMService.WMS_1_0_0.equals(version)) {
-      serverURL = serverURL
-          + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
-    } else {
-      serverURL = serverURL + "&SERVICE=WMS&REQUEST=GetLegendGraphic&VERSION="
-          + version;
+    public LegendRequest(WMService service, String name) {
+        super(service);
+        layerName = name;
     }
-    serverURL = serverURL + "&FORMAT=image/png&WIDTH=16&HEIGHT=16";
-    serverURL = serverURL
-        + "&legend_options=bgColor:0xFFFFEE;dpi:100;fontAntiAliasing:true;forceLabels:on";
 
-    serverURL = serverURL + "&LAYER=" + layerName;
-    
-    return new URL(serverURL);
-  }
+    public URL getURL() throws MalformedURLException {
+        String serverURL = service.getServerUrl();
+
+        String version = service.getVersion();
+        if (WMService.WMS_1_0_0.equals(version)) {
+            serverURL = serverURL
+                    + "REQUEST=GetLegendGraphic&feature_info&WMTVER=1.0.0";
+        } else {
+            serverURL = serverURL
+                    + "&SERVICE=WMS&REQUEST=GetLegendGraphic&VERSION="
+                    + version;
+        }
+        serverURL = serverURL + "&FORMAT=image/png&WIDTH=16&HEIGHT=16";
+        serverURL = serverURL
+                + "&legend_options=bgColor:0xFFFFEE;dpi:100;fontAntiAliasing:true;forceLabels:on";
+
+        serverURL = serverURL + "&LAYER=" + layerName;
+
+        return new URL(serverURL);
+    }
 
 }
