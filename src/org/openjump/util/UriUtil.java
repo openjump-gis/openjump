577c0cbe13ddef5f24c3d4e93c63a6938d7ffc39
Edgar Soldin <edso@users.sourceforge.net>
2015-12-31 13:32:07 +0000

    fix NPE in FeatureInfoUtil if no auth was used

--- src/org/openjump/util/UriUtil.java
+++ src/org/openjump/util/UriUtil.java
@@ -23,63 +23,66 @@ public final class UriUtil {
   /**
    * Create a URI to the entry within a ZIP file.
    * 
-   * @param file The ZIP file.
-   * @param entry The ZIP entry.
+   * @param file
+   *          The ZIP file.
+   * @param entry
+   *          The ZIP entry.
    * @return The URI.
    */
   public static URI createZipUri(final File file, final String entry) {
     try {
-      //final String entryName = entry.getName();
+      // final String entryName = entry.getName();
       final URI fileUri = file.toURI();
       final String filePath = fileUri.getPath();
       final URI uri = new URI("zip", null, filePath + "!/" + entry, null);
       return uri;
     } catch (final URISyntaxException e) {
       throw new IllegalArgumentException("Cannot create URI for " + file + "!/"
-        + entry);
+          + entry);
     }
   }
-  
+
   public static URI createZipUri(final String file, final String entry) {
     return createZipUri(new File(file), entry);
   }
-  
-  public static URI createFileUri( final String file ) {
+
+  public static URI createFileUri(final String file) {
     return new File(file).toURI();
   }
 
   /**
    * Get the ZIP file from a ZIP URI.
    * 
-   * @param uri The URI.
+   * @param uri
+   *          The URI.
    * @return The ZIP file.
    */
   public static File getZipFile(final URI uri) {
-      try {
-        final URI fileUri = new URI("file", null, getZipFilePath(uri),
-          null);
-        return new File(fileUri);
-      } catch (final URISyntaxException e) {
-        throw new RuntimeException(e.getMessage(), e);
-      }
+    try {
+      final URI fileUri = new URI("file", null, getZipFilePath(uri), null);
+      return new File(fileUri);
+    } catch (final URISyntaxException e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
   }
 
-  public static String getZipFilePath(final URI uri){
+  public static String getZipFilePath(final URI uri) {
     final String path = uri.getPath();
     final int index = path.indexOf('!');
-    return index<0 ? path : path.substring(0, index);
+    return index < 0 ? path : path.substring(0, index);
   }
 
-  public static String getZipFileName(final URI uri){
+  public static String getZipFileName(final URI uri) {
     final String path = getZipFilePath(uri);
     final int index = path.lastIndexOf('/');
-    return index<0 ? path : path.substring(index+1);
+    return index < 0 ? path : path.substring(index + 1);
   }
 
   /**
    * Get the name of a ZIP file entry from a ZIP URI.
    * 
-   * @param uri The URI.
+   * @param uri
+   *          The URI.
    * @return The ZIP entry name.
    */
   public static String getZipEntryName(final URI uri) {
@@ -95,7 +98,8 @@ public final class UriUtil {
   /**
    * Get the file extension from the URI path.
    * 
-   * @param uri The URI.
+   * @param uri
+   *          The URI.
    * @return The file extension.
    */
   public static String getFileExtension(final URI uri) {
@@ -111,7 +115,8 @@ public final class UriUtil {
   /**
    * Get the file name from the URI path.
    * 
-   * @param uri The URI.
+   * @param uri
+   *          The URI.
    * @return The file name.
    */
   public static String getFileName(final URI uri) {
@@ -122,7 +127,8 @@ public final class UriUtil {
   /**
    * Get the file name without the extension from the URI path.
    * 
-   * @param uri The URI.
+   * @param uri
+   *          The URI.
    * @return The file name.
    */
   public static String getFileNameWithoutExtension(final URI uri) {
@@ -138,7 +144,8 @@ public final class UriUtil {
   /**
    * Get the path to an entry in a ZIP URI.
    * 
-   * @param uri The zip URI
+   * @param uri
+   *          The zip URI
    * @return The entry's path.
    */
   public static String getZipEntryFilePath(final URI uri) {
@@ -150,65 +157,65 @@ public final class UriUtil {
       return null;
     }
   }
-  
+
   public static String getFilePath(final URI uri) {
     return getZipFilePath(uri);
   }
-  
+
   public static String getFileName(final String path) {
     final int slashIndex = path.lastIndexOf('/');
     return slashIndex > -1 ? path.substring(slashIndex) : path;
   }
-  
+
   public static String getPath(final String path) {
     final int slashIndex = path.lastIndexOf('/');
-    return slashIndex > -1 ? path.substring(0,slashIndex) : "";
+    return slashIndex > -1 ? path.substring(0, slashIndex) : "";
   }
-  
+
   public static String removeExtension(final String path) {
     final int slashIndex = path.lastIndexOf('/');
     final int dotIndex = path.lastIndexOf('.');
-    return slashIndex < dotIndex ? path.substring(0,dotIndex) : path;
+    return slashIndex < dotIndex ? path.substring(0, dotIndex) : path;
   }
-  
+
   final static String charSet = "UTF-8";
-  
-  public static String urlEncode( String in ){
+
+  public static String urlEncode(String in) {
     try {
       return URLEncoder.encode(in, charSet);
     } catch (Exception e) {
       return in;
     }
   }
-  
-  public static String urlDecode( String in ){
+
+  public static String urlDecode(String in) {
     try {
       return URLDecoder.decode(in, charSet);
     } catch (Exception e) {
       return in;
     }
   }
-  
+
   final static String regexp = "^([^:/]+://)(?:(([^@:/]*)(?:\\:([^@:/]*))?)@)?((.*?)(?::(\\d+))?(/.*))$";
-  
-  public static boolean isURL( String in ){
+
+  public static boolean isURL(String in) {
     Pattern p = Pattern.compile(regexp);
     Matcher m = p.matcher(in != null ? in : "");
-    
-//    while (m.find()) {
-//      System.out.println("g in:"+in);
-//      for (int i = 1; i <= m.groupCount(); i++) {
-//        System.out.println("g"+i+":"+m.group(i));
-//      }
-//    }
+
+    // while (m.find()) {
+    // System.out.println("g in:"+in);
+    // for (int i = 1; i <= m.groupCount(); i++) {
+    // System.out.println("g"+i+":"+m.group(i));
+    // }
+    // }
     return m.matches();
   }
-  
-  public static String urlStripAuth( String url ) {
+
+  public static String urlStripAuth(String url) {
     String clean = isURL(url) ? url.replaceFirst(regexp, "$1$5") : url;
     return clean;
   }
-  
+
   public static String urlGetUser(String url) {
     if (isURL(url))
       return urlDecode(url.replaceFirst(regexp, "$3"));
@@ -251,14 +258,15 @@ public final class UriUtil {
     return "";
   }
 
-  public static String urlStripPassword( String url ) {
+  public static String urlStripPassword(String url) {
     String user = urlGetUser(url);
     if (!user.isEmpty())
       user += "@";
-    String clean = isURL(url) ? url.replaceFirst(regexp, "$1"+user+"$5") : url;
+    String clean = isURL(url) ? url.replaceFirst(regexp, "$1" + user + "$5")
+        : url;
     return clean;
   }
-  
+
   public static String urlAddCredentials(String url, String user, String pass) {
     if (!isURL(url))
       return url;
@@ -282,6 +290,9 @@ public final class UriUtil {
    */
   public static String urlAddUserInfo(String url, String userinfo) {
     if (!isURL(url))
+      return url;
+
+    if (userinfo == null || userinfo.isEmpty())
       return url;
 
     return url.replaceFirst(regexp, "$1" + userinfo + "@$5");
