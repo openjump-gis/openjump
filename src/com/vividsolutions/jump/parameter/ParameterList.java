22320572ef9cb3a609b31badbead821fe6a573ff
Michael Michaud <michaudm@users.sourceforge.net>
2016-03-18 07:05:09 +0000

    Modernize parameter package code

--- src/com/vividsolutions/jump/parameter/ParameterList.java
+++ src/com/vividsolutions/jump/parameter/ParameterList.java
@@ -2,16 +2,16 @@ package com.vividsolutions.jump.parameter;
 
 import java.util.*;
 
-import com.vividsolutions.jts.util.Assert;
 import com.vividsolutions.jump.util.LangUtil;
 
 /**
  * A strongly-typed list of parameters for passing to a component
  */
 public class ParameterList {
+
     private ParameterListSchema schema;
 
-    private Map params = new HashMap();
+    private Map<String,Object> params = new HashMap<>();
 
     public ParameterList(ParameterListSchema schema) {
         initialize(schema);
@@ -19,8 +19,7 @@ public class ParameterList {
 
     public ParameterList(ParameterList other) {
         initialize(other.getSchema());
-        for (Iterator i = Arrays.asList(other.getSchema().getNames()).iterator(); i.hasNext(); ) {
-            String name = (String) i.next();
+        for (String name : Arrays.asList(other.getSchema().getNames())) {
             setParameter(name, other.getParameter(name));
         }
     }
@@ -40,17 +39,17 @@ public class ParameterList {
     }
 
     public boolean equals(Object obj) {
-        return equals((ParameterList) obj);
+        return obj != null &&
+                obj instanceof ParameterList &&
+                equals((ParameterList)obj);
     }
 
     private boolean equals(ParameterList other) {
         if (!schema.equals(other.schema)) {
             return false;
         }
-        for (Iterator i = params.keySet().iterator(); i.hasNext();) {
-            String name = (String) i.next();
-            if (!LangUtil.bothNullOrEqual(params.get(name), other.params
-                    .get(name))) {
+        for (String name : params.keySet()) {
+            if (!LangUtil.bothNullOrEqual(params.get(name), other.params.get(name))) {
                 return false;
             }
         }
@@ -69,12 +68,11 @@ public class ParameterList {
         return (String) params.get(name);
     }
 
-    public int getParameterInt(String name)
-    {
-      Object value = params.get(name);
-      if (value instanceof String)
-        return Integer.parseInt((String) value);
-      return ((Integer) params.get(name)).intValue();
+    public int getParameterInt(String name) {
+        Object value = params.get(name);
+        if (value instanceof String)
+            return Integer.parseInt((String) value);
+        return (Integer) params.get(name);
     }
 
 }
--- src/com/vividsolutions/jump/parameter/ParameterListSchema.java
+++ src/com/vividsolutions/jump/parameter/ParameterListSchema.java
@@ -9,46 +9,50 @@ import java.util.*;
  */
 public class ParameterListSchema {
 
-  private String[] paramNames;
-  private Class[] paramClasses;
-  private Map nameMap = new HashMap();
+    private String[] paramNames;
+    private Class[] paramClasses;
+    private Map<String,Integer> name2posMap = new HashMap<>();
 
-  public ParameterListSchema(String[] paramNames, Class[] paramClasses) {
-    initialize(paramNames, paramClasses);
-  }
-
-  protected ParameterListSchema initialize(String[] paramNames, Class[] paramClasses) {
-    this.paramNames = paramNames;
-    this.paramClasses = paramClasses;
-
-    for (int i = 0; i < paramNames.length; i++) {
-      nameMap.put(paramNames[i], new Integer(i));
+    public ParameterListSchema(String[] paramNames, Class[] paramClasses) {
+        initialize(paramNames, paramClasses);
     }
-    return this;
-  }
 
-  public String[] getNames()  { return paramNames; }
-  public Class[] getClasses()  { return paramClasses; }
-  public boolean isValidName(String name)
-  {
-    return nameMap.containsKey(name);
-  }
+    protected ParameterListSchema initialize(String[] paramNames, Class[] paramClasses) {
+        this.paramNames = paramNames;
+        this.paramClasses = paramClasses;
+
+        for (int i = 0; i < paramNames.length; i++) {
+          name2posMap.put(paramNames[i], i);
+        }
+        return this;
+    }
+
+    public String[] getNames()  { return paramNames; }
+
+    public Class[] getClasses()  { return paramClasses; }
+
+    public boolean isValidName(String name) {
+        return name2posMap.containsKey(name);
+    }
   
-  public boolean equals(Object obj) {      
-    return equals((ParameterListSchema)obj);
-  }
-  private boolean equals(ParameterListSchema other) {
-    if (paramNames.length != other.paramNames.length) { return false; }
-    for (int i = 0; i < paramNames.length; i++) {
-      if (!paramNames[i].equals(other.paramNames[i])) { return false; }    
+    public boolean equals(Object obj) {
+        return obj != null &&
+                obj instanceof ParameterListSchema &&
+                equals((ParameterListSchema) obj);
     }
-    for (int i = 0; i < paramNames.length; i++) {
-      if (paramClasses[i] != other.paramClasses[i]) { return false; }    
-    }    
-    return true;
-  }
 
-  public Class getClass(String name) {
-    return paramClasses[((Integer)nameMap.get(name)).intValue()];
-  }
+    private boolean equals(ParameterListSchema other) {
+        if (paramNames.length != other.paramNames.length) { return false; }
+        for (int i = 0; i < paramNames.length; i++) {
+            if (!paramNames[i].equals(other.paramNames[i])) { return false; }
+        }
+        for (int i = 0; i < paramNames.length; i++) {
+            if (paramClasses[i] != other.paramClasses[i]) { return false; }
+        }
+        return true;
+    }
+
+    public Class getClass(String name) {
+        return paramClasses[name2posMap.get(name)];
+    }
 }
