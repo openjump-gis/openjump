84619511553a6bce6e802c03ddcfc5645cdf6af4
Giuseppe Aruta <ma15569@users.sourceforge.net>
2016-07-05 15:11:41 +0000

    Add capability to read SRID from auxiliary file (prj or aux) or geotiff tag, if present

--- src/org/openjump/core/ui/plugin/layer/ChangeSRIDPlugIn.java
+++ src/org/openjump/core/ui/plugin/layer/ChangeSRIDPlugIn.java
@@ -3,6 +3,7 @@ package org.openjump.core.ui.plugin.layer;
 import javax.swing.JOptionPane;
 
 import org.openjump.core.ccordsys.srid.SRIDStyle;
+import org.openjump.core.ccordsys.utils.ProjUtils;
 
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.model.Layer;
@@ -17,42 +18,56 @@ import com.vividsolutions.jump.workbench.ui.plugin.FeatureInstaller;
 
 public class ChangeSRIDPlugIn extends AbstractPlugIn {
     public void initialize(PlugInContext context) throws Exception {
-        EnableCheckFactory enableCheckFactory = new EnableCheckFactory(context
-                .getWorkbenchContext());
+        EnableCheckFactory enableCheckFactory = new EnableCheckFactory(
+                context.getWorkbenchContext());
         EnableCheck enableCheck = new MultiEnableCheck().add(
                 enableCheckFactory
                         .createWindowWithLayerManagerMustBeActiveCheck()).add(
                 enableCheckFactory.createExactlyNLayersMustBeSelectedCheck(1));
-        new FeatureInstaller(context.getWorkbenchContext()).addMainMenuPlugin(this,
-                new String[]{MenuNames.LAYER}, getName() + "...", false, null, enableCheck);
+        new FeatureInstaller(context.getWorkbenchContext()).addMainMenuPlugin(
+                this, new String[] { MenuNames.LAYER }, getName() + "...",
+                false, null, enableCheck);
     }
-    
-    public String getName(){
-    	return I18N.get("org.openjump.core.ui.plugin.layer.ChangeSRIDPlugIn.Change-SRID"); 
+
+    public String getName() {
+        return I18N
+                .get("org.openjump.core.ui.plugin.layer.ChangeSRIDPlugIn.Change-SRID");
     }
-    
+
     public boolean execute(PlugInContext context) throws Exception {
         reportNothingToUndoYet(context);
         final Layer layer = context.getSelectedLayer(0);
         final SRIDStyle sridStyle = (SRIDStyle) layer.getStyle(SRIDStyle.class);
-        final int oldSRID = sridStyle.getSRID();
-        String input = (String) JOptionPane.showInputDialog(context
-                .getWorkbenchFrame(), "SRID:", getName(),
-                JOptionPane.PLAIN_MESSAGE, null, null, "" + oldSRID);
+        final int prjSRID = ProjUtils.SRID(layer);
+        String input = null;
+
+        input = (String) JOptionPane.showInputDialog(
+                context.getWorkbenchFrame(), "SRID:", getName(),
+                JOptionPane.PLAIN_MESSAGE, null, null, "" + prjSRID);
         if (input == null) {
             return false;
         }
         final int newSRID = Integer.parseInt(input);
         execute(new UndoableCommand(getName()) {
             public void execute() {
-                sridStyle.setSRID(newSRID);
-                sridStyle.updateSRIDs(layer);
+                if (newSRID != prjSRID) {
+                    sridStyle.setSRID(newSRID);
+                    sridStyle.updateSRIDs(layer);
+                } else if (newSRID == prjSRID) {
+                    // sridStyle.setSRID(oldSRID);
+                    sridStyle.setEnabled(false);
+                    layer.removeStyle(sridStyle);
+                    layer.fireAppearanceChanged();
+                }
+
             }
+
             public void unexecute() {
-                sridStyle.setSRID(oldSRID);
+                sridStyle.setSRID(prjSRID);
                 sridStyle.updateSRIDs(layer);
             }
         }, context);
         return true;
     }
+
 }
