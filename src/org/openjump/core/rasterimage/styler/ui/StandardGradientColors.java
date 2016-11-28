883e6406ef74c30a95e21cb1a91a2900aee50e90
Giuseppe Aruta <ma15569@users.sourceforge.net>
2016-11-28 15:46:42 +0000

    Added some more complex color maps for rasters

--- src/org/openjump/core/rasterimage/styler/ui/StandardGradientColors.java
+++ src/org/openjump/core/rasterimage/styler/ui/StandardGradientColors.java
@@ -70,6 +70,46 @@ public class StandardGradientColors {
             Color.decode("#aaaaaa"),
             Color.decode("#fffcff")}));
         
+     // BWBW
+        colorMaps_l.add(createRamp(new Color[] { Color.WHITE, Color.BLACK,
+                Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK,
+                Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK,
+                Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK,
+                Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK,
+                Color.WHITE, Color.BLACK }));
+        
+        // DEM-2
+        colorMaps_l.add(createRamp(new Color[] { Color.decode("#09114f"),
+                Color.decode("#017dff"), Color.decode("#009c01"),
+                Color.decode("#5dd331"), Color.decode("#02ff00"),
+                Color.decode("#ffff00"), Color.decode("#f6d700"),
+                Color.decode("#ff9b33"), Color.decode("#ed7306"),
+                Color.decode("#fa210e"), Color.decode("#d20005"),
+                Color.decode("#888e46"), Color.decode("#a0a64e"),
+                Color.decode("#c3c184"), Color.decode("#e4e1c2") }));
+        
+        // 36 colors
+        colorMaps_l.add(createRamp(new Color[] { Color.decode("#e6004d"),
+                Color.decode("#ff0000"), Color.decode("#cc4df2"),
+                Color.decode("#cc0000"), Color.decode("#e6cccc"),
+                Color.decode("#e6ccd8"), Color.decode("#a600cc"),
+                Color.decode("#a64d00"), Color.decode("#ff4dff"),
+                Color.decode("#ffa6ff"), Color.decode("#ffe6ff"),
+                Color.decode("#ffffa8"), Color.decode("#ffff00"),
+                Color.decode("#e6e600"), Color.decode("#e68000"),
+                Color.decode("#f2a64d"), Color.decode("#e6a600"),
+                Color.decode("#e6e64d"), Color.decode("#ffe6a6"),
+                Color.decode("#ffe64d"), Color.decode("#e6cc4d"),
+                Color.decode("#f2cca6"), Color.decode("#80ff00"),
+                Color.decode("#00a600"), Color.decode("#45ff00"),
+                Color.decode("#ccf24d"), Color.decode("#a6ff80"),
+                Color.decode("#a6e64d"), Color.decode("#a6f200"),
+                Color.decode("#e6e6e6"), Color.decode("#cccccc"),
+                Color.decode("#ccffcc"), Color.decode("#000000"),
+                Color.decode("#a6e6cc"), Color.decode("#a6a6ff"),
+                Color.decode("#4d4dff"),
+
+        }));
         return colorMaps_l;
         
     }
