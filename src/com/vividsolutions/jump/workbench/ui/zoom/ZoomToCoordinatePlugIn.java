a4fd4ab869d290cb72544c820438f737447fe150
Edgar Soldin <edso@users.sourceforge.net>
2016-12-29 18:16:49 +0000

    enhance and i18n error handling

--- src/com/vividsolutions/jump/workbench/ui/zoom/ZoomToCoordinatePlugIn.java
+++ src/com/vividsolutions/jump/workbench/ui/zoom/ZoomToCoordinatePlugIn.java
@@ -10,6 +10,7 @@ import javax.swing.JOptionPane;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jump.I18N;
+import com.vividsolutions.jump.JUMPException;
 import com.vividsolutions.jump.feature.Feature;
 import com.vividsolutions.jump.geom.EnvelopeUtil;
 import com.vividsolutions.jump.util.CoordinateArrays;
@@ -27,10 +28,29 @@ public class ZoomToCoordinatePlugIn extends AbstractPlugIn {
   private Coordinate lastCoordinate = new Coordinate(0, 0);
 
   public boolean execute(PlugInContext context) throws Exception {
-    Coordinate coordinate = prompt(context);
+    Coordinate coordinate = null;
+    boolean retry = true;
+    while (retry) {
+      try {
+        String value = JOptionPane
+            .showInputDialog(
+                context.getWorkbenchFrame(),
+                I18N.get("ui.zoom.ZoomToCoordinatePlugIn.enter-coordinate-to-zoom-to"),
+                lastCoordinate.x + ", " + lastCoordinate.y);
+        if (value == null) {
+          retry = false;
+        } else {
+          coordinate = toCoordinate(value);
+        }
+      } catch (Exception e) {
+        context.getWorkbenchContext().getErrorHandler().handleThrowable(e);
+      }
+    }
+
     if (coordinate == null) {
       return false;
     }
+
     lastCoordinate = coordinate;
     context.getLayerViewPanel().getViewport()
         .zoom(toEnvelope(coordinate, context.getLayerManager()));
@@ -39,22 +59,6 @@ public class ZoomToCoordinatePlugIn extends AbstractPlugIn {
         context.getLayerViewPanel(), new float[] { 20, 20 });
 
     return true;
-  }
-
-  private Coordinate prompt(PlugInContext context) {
-    while (true) {
-      try {
-        return toCoordinate(JOptionPane.showInputDialog(context
-            .getWorkbenchFrame(), I18N
-            .get("ui.zoom.ZoomToCoordinatePlugIn.enter-coordinate-to-zoom-to"),
-            lastCoordinate.x + ", " + lastCoordinate.y));
-      } catch (Exception e) {
-        JOptionPane.showMessageDialog(context.getWorkbenchFrame(),
-            e.getMessage(), context.getWorkbenchFrame().getTitle(),
-            JOptionPane.ERROR_MESSAGE);
-      }
-    }
-
   }
 
   private Envelope toEnvelope(Coordinate coordinate, LayerManager layerManager) {
@@ -90,22 +94,21 @@ public class ZoomToCoordinatePlugIn extends AbstractPlugIn {
   }
 
   private Coordinate toCoordinate(String s) throws Exception {
-    if (s == null) {
-      return null;
-    }
-    if (s.trim().length() == 0) {
-      return null;
-    }
     s = StringUtil.replaceAll(s, ",", " ");
     StringTokenizer tokenizer = new StringTokenizer(s);
+    if (tokenizer.countTokens() < 2) {
+      throw new JUMPException(
+          I18N.get("ui.zoom.ZoomToCoordinatePlugIn.enter-two-values"));
+    }
+
     String x = tokenizer.nextToken();
-    if (!StringUtil.isNumber(x)) {
-      throw new Exception("Not a number: " + x);
-    }
+    if (!StringUtil.isNumber(x))
+      throw new JUMPException(I18N.getMessage("ui.zoom.ZoomToCoordinatePlugIn.{0}-is-not-a-number", x));
+
     String y = tokenizer.nextToken();
-    if (!StringUtil.isNumber(y)) {
-      throw new Exception("Not a number: " + y);
-    }
+    if (!StringUtil.isNumber(y))
+      throw new JUMPException(I18N.getMessage("ui.zoom.ZoomToCoordinatePlugIn.{0}-is-not-a-number", y));
+
     return new Coordinate(Double.parseDouble(x), Double.parseDouble(y));
   }
 
