96374a2de51a0a49cc948cd3b58f08e427932134
Giuseppe Aruta <ma15569@users.sourceforge.net>
2017-04-06 17:00:54 +0000

    Patch to extend projection detection to SRIDStyle

--- src/org/openjump/core/ccordsys/utils/ProjUtils.java
+++ src/org/openjump/core/ccordsys/utils/ProjUtils.java
@@ -19,6 +19,8 @@ import org.openjump.core.rasterimage.TiffTags;
 
 
 
+import org.openjump.core.rasterimage.TiffTags.TiffReadingException;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -48,8 +50,13 @@ public class ProjUtils {
             .get("org.openjump.core.ui.plugin.layer.LayerPropertiesPlugIn.User_defined");
     private static final String NOT_RECOGNIZED = I18N
             .get("org.openjump.core.ui.plugin.layer.LayerPropertiesPlugIn.Not_recognized");
-
-
+    private static final String NO_SRS =I18N
+        .get("org.openjump.core.ccordsys.No-srs");
+    private static final String EMBEDDED_SRS =I18N
+        .get("org.openjump.core.ccordsys.Embedded-srs");    
+    private static final String NOT_CONSISTENT_SRS =I18N
+        .get("org.openjump.core.ccordsys.Not-consistent-srs");       
+   
     /**
      * - Read SRS from GeoTIFF tag - This method gets projection srid code and
      * projection info from a geotiff file. It first scans GeoKeyDirectoryTag to
@@ -227,6 +234,39 @@ public class ProjUtils {
         return prjname;
     }
 
+    /**
+     * Method to get SRID from a layer from Style or from auxiliary file. 
+     * It tries to check all possibilities:
+     * a) SRID is recorded from layer auxiliary file
+     * b) SRID is recorded from data source
+     * c) No SRID is available
+     * d) Layer SRID doesn't match auxilary file/datasource SRID
+     * @param layer
+     * @return SRID
+     * @throws Exception
+     */   
+    
+    public static SRSInfo getSRSInfoFromLayerStyleOrSource(Layer layer)
+        throws Exception {
+    // read SRID layer recorded as STYLE (for WFS, Databases and newly
+    // generated files)
+        SRIDStyle sridStyle = (SRIDStyle) layer.getStyle(SRIDStyle.class);
+        int SRID_style_int = sridStyle.getSRID();
+        String SRID_style_string = String.valueOf(SRID_style_int);
+    // read SRID layer from aux file or geotiff tag
+       SRSInfo srsInfo = ProjUtils.getSRSInfoFromLayerSource(layer);
+       String SRID_srs_string = srsInfo.getCode();
+       srsInfo.setCode(SRID_style_string);
+       if (SRID_srs_string.equals("0") & SRID_style_string.equals("0"))
+           srsInfo.setSource(NO_SRS);
+       if ((!SRID_style_string.equals(SRID_srs_string) & SRID_srs_string.equals("0") & !SRID_style_string.equals("0")) || isImageGeoTIFFLayer(layer) ) 
+           srsInfo.setSource(EMBEDDED_SRS);
+      if (!SRID_style_string.equals(SRID_srs_string) & !SRID_style_string.equals("0") & !SRID_srs_string.equals("0"))
+           srsInfo.setSource(NOT_CONSISTENT_SRS);
+        srsInfo.complete();
+        return srsInfo;
+    }
+    
 
     /**
      * Method to get SRID from a layer from Style or from auxiliary file. First
@@ -237,7 +277,7 @@ public class ProjUtils {
      * @return SRID
      * @throws Exception
      */
-    public static SRSInfo getSRSInfoFromLayerStyleOrSource(Layer layer) throws Exception {
+    public static SRSInfo getSRSInfoFromLayerStyleOrSource_old(Layer layer) throws Exception {
         SRSInfo srsInfo = new SRSInfo();
 
         // First we check if a SRID != 0 has already been recorded for this Layer
@@ -255,7 +295,7 @@ public class ProjUtils {
         return srsInfo;
 
     }
-    //[Giuseppe Aruta 28(3/2017] small patch to correct reading SRID from aux file
+    
     /**
      * Method to get SRID from a layer file from auxiliary files (.prj or .aux)
      * or GeoTIFFed tag. If the auxiliary file SRID does not exist, it returns
@@ -340,6 +380,47 @@ public class ProjUtils {
         }
     }
 
+    
+    // Boolean. Selected layer is related to GEoTIFF file
+    public static boolean isImageGeoTIFFLayer(Layer layer) {
+        // String sclass = layer.getClass().getSimpleName();
+        // if (sclass.equals("ReferencedImagesLayer"))
+        String fileSourcePath = "";
+        if (layer.getStyle(ReferencedImageStyle.class) != null
+                && (layer.getDescription() != null)) {
+
+            FeatureCollection featureCollection = layer
+                    .getFeatureCollectionWrapper();
+            String sourcePathImage = null;
+            for (Iterator i = featureCollection.iterator(); i.hasNext();) {
+                Feature feature = (Feature) i.next();
+                sourcePathImage = feature
+                        .getString(ImageryLayerDataset.ATTR_URI);
+
+                if (sourcePathImage != null && !sourcePathImage.isEmpty()) {
+                    File f = new File(URI.create(sourcePathImage).getPath());
+                    if (f.exists()) {
+                        fileSourcePath = f.getAbsolutePath();
+                        break;
+                    }
+                }
+            }
+            TiffTags.TiffMetadata metadata = null;
+            try {
+                metadata = TiffTags.readMetadata(new File(fileSourcePath));
+            } catch (ImageReadException | IOException | TiffReadingException e) {
+                e.printStackTrace();
+            }
+            if (metadata.isGeoTiff()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    
     //[Giuseppe Aruta 04/01/2017] get SRS info for RasterImageLayer.class
     public static SRSInfo getSRSInfoFromLayerSource(RasterImageLayer layer)
         throws Exception, URISyntaxException {
