0ee8afa54c844a12b3ddaa42d0519590b2eb2b4d
Nicolas Ribot <elnico@users.sourceforge.net>
2017-06-01 10:32:24 +0000

    PostgreSQL JDBC driver : set the ApplicationName driver's property to allow OpenJUMP to advertise its name to PostgreSQL backend

--- src/com/vividsolutions/jump/datastore/postgis/PostgisDataStoreDriver.java
+++ src/com/vividsolutions/jump/datastore/postgis/PostgisDataStoreDriver.java
@@ -1,10 +1,13 @@
 package com.vividsolutions.jump.datastore.postgis;
 
+import com.vividsolutions.jump.I18N;
+import com.vividsolutions.jump.JUMPVersion;
 import java.sql.Connection;
 
 import com.vividsolutions.jump.datastore.DataStoreConnection;
 import com.vividsolutions.jump.datastore.spatialdatabases.AbstractSpatialDatabasesDSDriver;
 import com.vividsolutions.jump.parameter.ParameterList;
+import java.util.Properties;
 
 /**
  * A driver for supplying {@link com.vividsolutions.jump.datastore.spatialdatabases.SpatialDatabasesDSConnection}s
@@ -29,7 +32,13 @@ public class PostgisDataStoreDriver
     @Override
     public DataStoreConnection createConnection(ParameterList params)
         throws Exception {
-        Connection conn = super.createJdbcConnection(params);
+        // Adds custom PG JDBC driver property: ApplicationName: useful to monitor applications connected to a PG instance
+        // cf: https://jdbc.postgresql.org/documentation/head/connect.html
+        Properties connectionProps = new Properties();
+        connectionProps.put(
+            "ApplicationName", 
+            I18N.get("JUMPWorkbench.jump") + " " + JUMPVersion.CURRENT_VERSION);
+        Connection conn = super.createJdbcConnection(params, connectionProps);
         return new PostgisDSConnection(conn);
     }
 }
