ba3c0d29c2984c3c94709475a2ab8f6dfe7bc3d9
Giuseppe Aruta <ma15569@users.sourceforge.net>
2017-09-23 05:56:16 +0000

    Added Syncronize windows zoom (only) tool

--- src/org/openjump/core/ui/plugin/window/SynchronizationPlugIn.java
+++ src/org/openjump/core/ui/plugin/window/SynchronizationPlugIn.java
@@ -25,7 +25,16 @@
 
 package org.openjump.core.ui.plugin.window;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JInternalFrame;
+import javax.swing.JRadioButtonMenuItem;
+
 import org.openjump.core.ui.plugin.AbstractUiPlugIn;
+
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.WorkbenchContext;
@@ -38,152 +47,259 @@ import com.vividsolutions.jump.workbench.ui.TaskFrame;
 import com.vividsolutions.jump.workbench.ui.Viewport;
 import com.vividsolutions.jump.workbench.ui.ViewportListener;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Point2D;
-import javax.swing.ButtonGroup;
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JInternalFrame;
-
 /**
  * A plugin to synchronize / desynchronize LayerViewPanels.
  * 
  * @author Michael Michaud
  * @version 0.1 (2008-04-06)
  * @since 1.2F
+ * 
+ *        Giuseppe Aruta [2017-09-23] Added Syncronize zoom only
  */
 public class SynchronizationPlugIn extends AbstractUiPlugIn {
 
     static ViewportListener vpl;
 
     public SynchronizationPlugIn() {
-      super();
+        super();
     }
 
     public SynchronizationPlugIn(String name) {
         super(name);
     }
 
+    @Override
     public void initialize(final PlugInContext context) throws Exception {
 
         // Set the workbenchContext used in getEnableCheck method
         super.initialize(context);
 
-        final JRadioButtonMenuItem desynchronizeMI =
-            new JRadioButtonMenuItem(I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Desynchronize"));
-        final JRadioButtonMenuItem synchronizePanMI =
-            new JRadioButtonMenuItem(I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronize-pan-only"));
-        final JRadioButtonMenuItem synchronizeAllMI =
-            new JRadioButtonMenuItem(I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronize-pan-and-zoom"));
+        final JRadioButtonMenuItem desynchronizeMI = new JRadioButtonMenuItem(
+                I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Desynchronize"));
+        final JRadioButtonMenuItem synchronizePanMI = new JRadioButtonMenuItem(
+                I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronize-pan-only"));
+        final JRadioButtonMenuItem synchronizeZoomMI = new JRadioButtonMenuItem(
+                I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronize-zoom-only"));
+        final JRadioButtonMenuItem synchronizeAllMI = new JRadioButtonMenuItem(
+                I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronize-pan-and-zoom"));
 
         ButtonGroup bgroup = new ButtonGroup();
         bgroup.add(desynchronizeMI);
         bgroup.add(synchronizePanMI);
+        bgroup.add(synchronizeZoomMI);
         bgroup.add(synchronizeAllMI);
-        
+
         desynchronizeMI.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {desynchronize();}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                desynchronize();
+            }
         });
         synchronizePanMI.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {synchronize(false);}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                synchronize(false);
+            }
         });
         synchronizeAllMI.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {synchronize(true);}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                synchronize(true);
+            }
+        });
+        synchronizeZoomMI.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                synchronizeZoom(true);
+            }
         });
 
         context.getFeatureInstaller().addMainMenuItem(
-            new String[]{MenuNames.WINDOW, MenuNames.WINDOW_SYNCHRONIZATION},
-            this,
-            synchronizePanMI,
-            0);
+                new String[] { MenuNames.WINDOW,
+                        MenuNames.WINDOW_SYNCHRONIZATION }, this,
+                synchronizePanMI, 0);
         context.getFeatureInstaller().addMainMenuItem(
-            new String[]{MenuNames.WINDOW, MenuNames.WINDOW_SYNCHRONIZATION},
-            this,
-            synchronizeAllMI,
-            1);
+                new String[] { MenuNames.WINDOW,
+                        MenuNames.WINDOW_SYNCHRONIZATION }, this,
+                synchronizeZoomMI, 1);
         context.getFeatureInstaller().addMainMenuItem(
-            new String[]{MenuNames.WINDOW, MenuNames.WINDOW_SYNCHRONIZATION},
-            this,
-            desynchronizeMI,
-            2);
+                new String[] { MenuNames.WINDOW,
+                        MenuNames.WINDOW_SYNCHRONIZATION }, this,
+                synchronizeAllMI, 2);
+        context.getFeatureInstaller().addMainMenuItem(
+                new String[] { MenuNames.WINDOW,
+                        MenuNames.WINDOW_SYNCHRONIZATION }, this,
+                desynchronizeMI, 3);
 
     }
 
+    @Override
     public String getName() {
-        return I18N.get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronization");
+        return I18N
+                .get("org.openjump.core.ui.plugin.window.SyncronizationPlugIn.Synchronization");
     }
 
+    @Override
     public EnableCheck getEnableCheck() {
-        EnableCheckFactory checkFactory = new EnableCheckFactory(workbenchContext);
-        return new MultiEnableCheck()
-            .add(checkFactory.createWindowWithLayerViewPanelMustBeActiveCheck());
+        EnableCheckFactory checkFactory = new EnableCheckFactory(
+                workbenchContext);
+        return new MultiEnableCheck().add(checkFactory
+                .createWindowWithLayerViewPanelMustBeActiveCheck());
     }
 
     public void synchronize(boolean panAndZoom) {
         ViewportListener oldViewportListener = vpl;
         vpl = createViewportListener(panAndZoom);
         // add the listener to every active map window
-        JInternalFrame[] iframes = workbenchContext.getWorkbench().getFrame().getInternalFrames();
-            for (JInternalFrame iframe : iframes) {
-                if (iframe instanceof TaskFrame) {
-                    // Remove the old listener if any before adding a new one
-                    // Thus, one can reinitialize synchronization if the number
-                    // of map windows change
-                    // Ideally, one should intercept new window creation or window focus
-                    // change to add new listeners to new windows..
-                    ((TaskFrame)iframe).getLayerViewPanel().getViewport().removeListener(oldViewportListener);
-                    ((TaskFrame)iframe).getLayerViewPanel().getViewport().addListener(vpl);
-                }
+        JInternalFrame[] iframes = workbenchContext.getWorkbench().getFrame()
+                .getInternalFrames();
+        for (JInternalFrame iframe : iframes) {
+            if (iframe instanceof TaskFrame) {
+                // Remove the old listener if any before adding a new one
+                // Thus, one can reinitialize synchronization if the number
+                // of map windows change
+                // Ideally, one should intercept new window creation or window
+                // focus
+                // change to add new listeners to new windows..
+                ((TaskFrame) iframe).getLayerViewPanel().getViewport()
+                        .removeListener(oldViewportListener);
+                ((TaskFrame) iframe).getLayerViewPanel().getViewport()
+                        .addListener(vpl);
             }
+        }
+    }
+
+    public void synchronizeZoom(boolean zoom) {
+        ViewportListener oldViewportListener = vpl;
+        vpl = createViewportListenerZoom(zoom);
+        // add the listener to every active map window
+        JInternalFrame[] iframes = workbenchContext.getWorkbench().getFrame()
+                .getInternalFrames();
+        for (JInternalFrame iframe : iframes) {
+            if (iframe instanceof TaskFrame) {
+                // Remove the old listener if any before adding a new one
+                // Thus, one can reinitialize synchronization if the number
+                // of map windows change
+                // Ideally, one should intercept new window creation or window
+                // focus
+                // change to add new listeners to new windows..
+                ((TaskFrame) iframe).getLayerViewPanel().getViewport()
+                        .removeListener(oldViewportListener);
+                ((TaskFrame) iframe).getLayerViewPanel().getViewport()
+                        .addListener(vpl);
+
+            }
+        }
     }
 
     public void desynchronize() {
         ViewportListener oldViewPortListener = vpl;
         vpl = null;
         // add the listener to every active map window
-        JInternalFrame[] iframes = workbenchContext.getWorkbench().getFrame().getInternalFrames();
+        JInternalFrame[] iframes = workbenchContext.getWorkbench().getFrame()
+                .getInternalFrames();
         for (JInternalFrame iframe : iframes) {
             if (iframe instanceof TaskFrame) {
                 // Remove the old listener if any before adding a new one
                 // Thus, one can reinitialize synchronization if the number
                 // of map windows change
-                // Ideally, one should intercept new window creation or window focus
+                // Ideally, one should intercept new window creation or window
+                // focus
                 // change to add new listeners to new windows..
-                ((TaskFrame)iframe).getLayerViewPanel().getViewport().removeListener(oldViewPortListener);
+                ((TaskFrame) iframe).getLayerViewPanel().getViewport()
+                        .removeListener(oldViewPortListener);
             }
         }
     }
 
-    private ViewportListener createViewportListener(final boolean panAndZoom) {
+    private ViewportListener createViewportListener(final boolean zoom) {
         final WorkbenchContext context = workbenchContext;
         return new ViewportListener() {
+            @Override
             public void zoomChanged(Envelope modelEnvelope) {
-                JInternalFrame[] iframes = context.getWorkbench().getFrame().getInternalFrames();
+                JInternalFrame[] iframes = context.getWorkbench().getFrame()
+                        .getInternalFrames();
                 for (JInternalFrame iframe : iframes) {
-                    if (iframe instanceof TaskFrame &&
-                        ((TaskFrame)iframe).getLayerViewPanel().getViewport() !=
-                        context.getLayerViewPanel().getViewport()) {
+                    if (iframe instanceof TaskFrame
+                            && ((TaskFrame) iframe).getLayerViewPanel()
+                                    .getViewport() != context
+                                    .getLayerViewPanel().getViewport()) {
                         try {
                             // Copy of method viewport.zoom(envelope)
-                            // without the statement fireZoomChanged(modelEnvelope)
+                            // without the statement
+                            // fireZoomChanged(modelEnvelope)
                             // to avoid entering an infinite loop
-                            // window 1 change --> window 2 change --> window 1 change ...
-                            Viewport viewport = ((TaskFrame)iframe).getLayerViewPanel().getViewport();
+                            // window 1 change --> window 2 change --> window 1
+                            // change ...
+                            Viewport viewport = ((TaskFrame) iframe)
+                                    .getLayerViewPanel().getViewport();
                             double w = viewport.getPanel().getWidth();
                             double h = viewport.getPanel().getHeight();
                             double scale = viewport.getScale();
+                            if (zoom) {
+                                scale = Math.min(w / modelEnvelope.getWidth(),
+                                        h / modelEnvelope.getHeight());
+                            }
+                            double xCenteringOffset = ((w / scale) - modelEnvelope
+                                    .getWidth()) / 2.0;
+                            double yCenteringOffset = ((h / scale) - modelEnvelope
+                                    .getHeight()) / 2.0;
+                            Point2D.Double viewOriginAsPerceivedByModel = new Point2D.Double(
+                                    modelEnvelope.getMinX() - xCenteringOffset,
+                                    modelEnvelope.getMinY() - yCenteringOffset);
+                            viewport.initialize(scale,
+                                    viewOriginAsPerceivedByModel);
+                            viewport.update();
+                        } catch (Exception e) {
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    private ViewportListener createViewportListenerZoom(final boolean panAndZoom) {
+        final WorkbenchContext context = workbenchContext;
+        return new ViewportListener() {
+            @Override
+            public void zoomChanged(Envelope modelEnvelope) {
+                JInternalFrame[] iframes = context.getWorkbench().getFrame()
+                        .getInternalFrames();
+                for (JInternalFrame iframe : iframes) {
+                    if (iframe instanceof TaskFrame
+                            && ((TaskFrame) iframe).getLayerViewPanel()
+                                    .getViewport() != context
+                                    .getLayerViewPanel().getViewport()) {
+                        try {
+                            // Copy of method viewport.zoom(envelope)
+                            // without the statement
+                            // fireZoomChanged(modelEnvelope)
+                            // to avoid entering an infinite loop
+                            // window 1 change --> window 2 change --> window 1
+                            // change ...
+                            Viewport viewport = ((TaskFrame) iframe)
+                                    .getLayerViewPanel().getViewport();
+                            double w = viewport.getPanel().getWidth();
+                            double h = viewport.getPanel().getHeight();
+                            double scale = viewport.getScale();
+
                             if (panAndZoom) {
                                 scale = Math.min(w / modelEnvelope.getWidth(),
-                                                 h / modelEnvelope.getHeight());
+                                        h / modelEnvelope.getHeight());
                             }
-                            double xCenteringOffset = ((w / scale) - modelEnvelope.getWidth()) / 2.0; 
-                            double yCenteringOffset = ((h / scale) - modelEnvelope.getHeight()) / 2.0; 
-                            Point2D.Double viewOriginAsPerceivedByModel =
-                                new Point2D.Double(modelEnvelope.getMinX() - xCenteringOffset,
+
+                            double xCenteringOffset = ((w / scale) - modelEnvelope
+                                    .getWidth()) / 2.0;
+                            double yCenteringOffset = ((h / scale) - modelEnvelope
+                                    .getHeight()) / 2.0;
+                            Point2D.Double viewOriginAsPerceivedByModel = new Point2D.Double(
+                                    modelEnvelope.getMinX() - xCenteringOffset,
                                     modelEnvelope.getMinY() - yCenteringOffset);
-                            viewport.initialize(scale, viewOriginAsPerceivedByModel);
+                            viewport.initialize(scale,
+                                    viewport.getOriginInModelCoordinates());
                             viewport.update();
-                        } catch(Exception e) {}
+                        } catch (Exception e) {
+                        }
                     }
                 }
             }
@@ -191,10 +307,11 @@ public class SynchronizationPlugIn extends AbstractUiPlugIn {
     }
 
     /**
-     * For this plugin, this method is unused
-     * All the work is defined in action listeners
+     * For this plugin, this method is unused All the work is defined in action
+     * listeners
      */
-    public boolean execute(PlugInContext context) throws Exception{
+    @Override
+    public boolean execute(PlugInContext context) throws Exception {
         return true;
     }
 
