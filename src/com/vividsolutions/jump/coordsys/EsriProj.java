3e731993c6908a8cf02ee1f175786daf6a948aae
Michaël Michaud <m.michael.michaud@orange.fr>
2022-03-27 11:52:42 +0200

    EsriPrj documentation

--- src/com/vividsolutions/jump/coordsys/EsriProj.java
+++ src/com/vividsolutions/jump/coordsys/EsriProj.java
@@ -8,6 +8,28 @@ import java.io.*;
 import java.net.URISyntaxException;
 import java.util.*;
 
+/**
+ * This class is used to find the precise Esri prj string for a given srid.
+ *
+ * Prj strings are taken from https://github.com/Esri/projection-engine-db-doc,
+ * licensed under the Apache License, Version 2.0 (the "License").
+ *
+ * This class uses the two following files from the github repository :
+ * <ul>
+ *   <li>/csv/pe_list_projcs.csv</li>
+ *   <li>/csv/pe_list_geogcs.csv</li>
+ * </ul>
+ * The two files have been concatenated in a single file and zipped in a single file
+ * containing a single entry named pe_list_projcs_geogcs.csv.
+ *
+ * The file is not decompressed during installation process. Searching a prj from an id
+ * fast enough (< 1s including the decompression step). Once a prj has been searched, it
+ * is cached in a map and the second search takes no time.
+ *
+ * There is room for improvement : the Esri file contains also bounding boxes for each
+ * projection and the wkt2 version of its description. A reverse search function (from
+ * string to id) could be implemented).
+ */
 public class EsriProj {
 
   // A cache to remember already used projection string
