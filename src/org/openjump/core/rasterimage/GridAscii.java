b1d98a1807aad926fb948ae313483a03bafb40db
Giuseppe Aruta <69568435+ma15569@users.noreply.github.com>
2023-04-28 07:13:08 +0200

    Update GridAscii.java

--- src/org/openjump/core/rasterimage/GridAscii.java
+++ src/org/openjump/core/rasterimage/GridAscii.java
@@ -445,11 +445,11 @@ public class GridAscii {
     }
     
     public Envelope getEnvelope() {
-    	Coordinate upperLeft = new Coordinate( xllCorner,
-    			yllCorner +  nRows *  cellSize);
-		Coordinate lowerRight = new Coordinate( xllCorner
-				+  nCols *  cellSize,  yllCorner);
-		return new Envelope(upperLeft, lowerRight);
+        Coordinate upperLeft = new Coordinate( xllCorner,
+              yllCorner +  nRows *  cellSize);
+        Coordinate lowerRight = new Coordinate( xllCorner
+              +  nCols *  cellSize,  yllCorner);
+        return new Envelope(upperLeft, lowerRight);
     }
     
     private String ascFullFileName = null;
